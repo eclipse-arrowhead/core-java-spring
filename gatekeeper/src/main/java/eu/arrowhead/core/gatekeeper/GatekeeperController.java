c4b5d571e20691f95e93f59e93c49701b7479a4e
borditamas <borditamas@gmail.com>
2019-05-22 13:56:04 +0200

    gatekeeper basic config

---
+++ gatekeeper/src/main/java/eu/arrowhead/core/gatekeeper/GatekeeperController.java
@@ -0,0 +1,19 @@
+package eu.arrowhead.core.gatekeeper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GatekeeperController {
+	
+	@Value("${sr_port}")
+	private long srPort;
+	
+	@RequestMapping(method = { RequestMethod.GET }, name = "/")
+	public String echoTimeout() {
+		System.out.println(srPort);
+		return String.valueOf(srPort);
+	}
+}
