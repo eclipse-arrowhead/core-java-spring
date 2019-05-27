088435868092f31d6eee598da99d451463721565
borditamas <borditamas@gmail.com>
2019-05-27 16:48:35 +0200

    Create empty arrowhead db script update

--- scripts/create_empty_arrowhead_db.sql
+++ scripts/create_empty_arrowhead_db.sql
@@ -4,9 +4,9 @@ USE `arrowhead`;
 # Dump of table system
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `system`;
+DROP TABLE IF EXISTS `system_`;
 
-CREATE TABLE `system` (
+CREATE TABLE `system_` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `system_name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
@@ -38,11 +38,11 @@ DROP TABLE IF EXISTS `service_interface`;
 
 CREATE TABLE `service_interface` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `interface` varchar(255) DEFAULT NULL,
+  `interface_` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `interface` (`interface`)
+  UNIQUE KEY `interface` (`interface_`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -65,9 +65,9 @@ CREATE TABLE `service_registry` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `pair` (`service_id`,`system_id`),
-  KEY `system` (`system_id`),
+  KEY `system_` (`system_id`),
   CONSTRAINT `service` FOREIGN KEY (`service_id`) REFERENCES `service_definition` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `system` FOREIGN KEY (`system_id`) REFERENCES `system` (`id`) ON DELETE CASCADE
+  CONSTRAINT `system` FOREIGN KEY (`system_id`) REFERENCES `system_` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -85,14 +85,14 @@ CREATE TABLE `service_registry_interface_connection` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `pair` (`service_registry_id`,`interface_id`),
-  KEY `interface` (`interface_id`),
-  CONSTRAINT `interface` FOREIGN KEY (`interface_id`) REFERENCES `service_interface` (`id`),
+  KEY `interface_` (`interface_id`),
+  CONSTRAINT `interface_` FOREIGN KEY (`interface_id`) REFERENCES `service_interface` (`id`),
   CONSTRAINT `service_registry` FOREIGN KEY (`service_registry_id`) REFERENCES `service_registry` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #DROP TABLE IF EXISTS `cloud`;
 CREATE TABLE `cloud` (
-  `id` bigint(20) AUTO_INCREMENT PRIMARY KEY,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `operator` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
@@ -104,63 +104,71 @@ CREATE TABLE `cloud` (
   `own_cloud` int(1) NOT NULL DEFAULT 0 COMMENT 'Is own cloud?',
   `created_at` timestamp NOT NULL DEFAULT NOW(),
   `updated_at` timestamp NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `cloud` (`operator`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #DROP TABLE IF EXISTS `intra_cloud_authorization`;
 CREATE TABLE `intra_cloud_authorization` (
-  `id` bigint(20) AUTO_INCREMENT PRIMARY KEY,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `consumer_system_id` bigint(20) NOT NULL,
   `provider_system_id` bigint(20) NOT NULL,
   `service_id` bigint(20) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT NOW(),
-  `updated_at` timestamp NOT NULL DEFAULT NOW() ON UPDATE NOW(),
-  UNIQUE KEY `rule` (`consumer_system_id`,`provider_system_id`,`service_id`),  
-  CONSTRAINT `service` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`),
-  CONSTRAINT `consumer` FOREIGN KEY (`consumer_system_id`) REFERENCES `system` (`id`),
-  CONSTRAINT `provider` FOREIGN KEY (`provider_system_id`) REFERENCES `system` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rule` (`consumer_system_id`,`provider_system_id`,`service_id`),
+  KEY `provider` (`provider_system_id`),
+  KEY `service_intra_auth` (`service_id`),
+  CONSTRAINT `service_intra_auth` FOREIGN KEY (`service_id`) REFERENCES `service_definition` (`id`),
+  CONSTRAINT `provider` FOREIGN KEY (`provider_system_id`) REFERENCES `system_` (`id`),
+  CONSTRAINT `consumer` FOREIGN KEY (`consumer_system_id`) REFERENCES `system_` (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #DROP TABLE IF EXISTS `inter_cloud_authorization`;
 CREATE TABLE `inter_cloud_authorization` (
-  `id` bigint(20) AUTO_INCREMENT PRIMARY KEY,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `consumer_cloud_id` bigint(20) NOT NULL,
   `service_id` bigint(20) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT NOW(),
-  `updated_at` timestamp NOT NULL DEFAULT NOW() ON UPDATE NOW(),
-  UNIQUE KEY `pair` (`consumer_cloud_id`,`arrowhead_service_id`),
-  CONSTRAINT `service` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`),
-  CONSTRAINT `cloud` FOREIGN KEY (`consumer_cloud_id`) REFERENCES `cloud` (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rule` (`consumer_cloud_id`,`service_id`),
+  KEY `service_inter_auth` (`service_id`),
+  CONSTRAINT `cloud` FOREIGN KEY (`consumer_cloud_id`) REFERENCES `cloud` (`id`),
+  CONSTRAINT `service_inter_auth` FOREIGN KEY (`service_id`) REFERENCES `service_definition` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 #DROP TABLE IF EXISTS `relay`;
 CREATE TABLE `relay` (
-  `id` bigint(20) AUTO_INCREMENT PRIMARY KEY,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `address` varchar(255) NOT NULL,
   `port` int(11) NOT NULL,
   `secure` int(1) NOT NULL DEFAULT 0,
   `created_at` timestamp NOT NULL DEFAULT NOW(),
   `updated_at` timestamp NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `pair` (`address`, `port`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #DROP TABLE IF EXISTS `orchestration_store`;
 CREATE TABLE `orchestration_store` (
-  `id` bigint(20) AUTO_INCREMENT PRIMARY KEY,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `consumer_system_id` bigint(20) NOT NULL,
   `provider_cloud_id` bigint(20) DEFAULT NULL,
   `provider_system_id` bigint(20) NOT NULL,
   `service_id` bigint(20) NOT NULL,
   `priority` int(11) DEFAULT NULL,
-  `attribute` text DEFAULT '',
+  `attribute` text,
   `created_at` timestamp NOT NULL DEFAULT NOW(),
   `updated_at` timestamp NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `unique` (`service_id`,`consumer_system_id`,`priority`),
-  CONSTRAINT `provider` FOREIGN KEY (`provider_system_id`) REFERENCES `system` (`id`),
-  CONSTRAINT `cloud` FOREIGN KEY (`provider_cloud_id`) REFERENCES `cloud` (`id`),
-  CONSTRAINT `consumer` FOREIGN KEY (`consumer_system_id`) REFERENCES `system` (`id`),
-  CONSTRAINT `service` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`)
+  CONSTRAINT `provider_orch` FOREIGN KEY (`provider_system_id`) REFERENCES `system_` (`id`),
+  CONSTRAINT `cloud_orch` FOREIGN KEY (`provider_cloud_id`) REFERENCES `cloud` (`id`),
+  CONSTRAINT `consumer_orch` FOREIGN KEY (`consumer_system_id`) REFERENCES `system_` (`id`),
+  CONSTRAINT `service_orch` FOREIGN KEY (`service_id`) REFERENCES `service_definition` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #DROP TABLE IF EXISTS `logs`;
