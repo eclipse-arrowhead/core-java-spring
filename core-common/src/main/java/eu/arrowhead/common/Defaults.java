8e88719071a883f46ca44ab7ac48bbe745804901
borditamas <borditamas@gmail.com>
2019-05-29 10:25:10 +0200

    Logs entity implemented

--- core-common/src/main/java/eu/arrowhead/common/Defaults.java
+++ core-common/src/main/java/eu/arrowhead/common/Defaults.java
@@ -2,6 +2,7 @@ package eu.arrowhead.common;
 
 public class Defaults {
 	
+	public static final int VARCHAR_LOG = 100;
 	public static final int VARCHAR_BASIC = 255;
 	public static final int VARCHAR_EXTENDED = 2047;
 
