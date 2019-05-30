68c817d78bdc4ea4b1bc5c3c451f4781b740a113
Gabor Majoros <gabormajo@yahoo.com>
2019-05-30 15:54:19 +0200

    add parameter not null assertion in convertSystemToSystemResponseDTO at DTOConverter

--- core-common/src/main/java/eu/arrowhead/common/dto/DTOConverter.java
+++ core-common/src/main/java/eu/arrowhead/common/dto/DTOConverter.java
@@ -1,6 +1,7 @@
 package eu.arrowhead.common.dto;
 
 import eu.arrowhead.common.database.entity.System;
+import org.springframework.util.Assert;
 
 public class DTOConverter {
 
@@ -10,7 +11,8 @@ public class DTOConverter {
 	
 	//-------------------------------------------------------------------------------------------------
 	public static SystemResponseDTO convertSystemToSystemResponseDTO(System system) {
-
+		Assert.notNull(system, "System is null");
+		
 		return new SystemResponseDTO(system.getSystemName(), system.getAddress(), system.getPort(), system.getAuthenticationInfo());		
 	}
 	
