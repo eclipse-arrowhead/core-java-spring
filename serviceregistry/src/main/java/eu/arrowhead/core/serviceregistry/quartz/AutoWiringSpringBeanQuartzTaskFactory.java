052c75c1cc0c033ff40893c08bc6d5beba8c4a33
borditamas <borditamas@gmail.com>
2019-05-31 14:52:44 +0200

    Quartz config update

--- serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/quartz/AutoWiringSpringBeanQuartzTaskFactory.java
+++ serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/quartz/AutoWiringSpringBeanQuartzTaskFactory.java
@@ -4,10 +4,9 @@ import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
-public final class AutoWiringSpringBeanQuartzTaskFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+public final class AutoWiringSpringBeanQuartzTaskFactory extends SpringBeanJobFactory {
 
     private transient AutowireCapableBeanFactory beanFactory;
 
