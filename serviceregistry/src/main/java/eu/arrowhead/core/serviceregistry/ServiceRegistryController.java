6f59ff073f80380ed084e3650ac95b2bd9d6e88d
Gabor Majoros <gabormajo@yahoo.com>
2019-05-31 14:10:33 +0200

    removed unused import at ServiceRegistryController

--- serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/ServiceRegistryController.java
+++ serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/ServiceRegistryController.java
@@ -6,7 +6,6 @@ import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
