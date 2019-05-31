021e8209b9dd40d48a87a688c2cd3c31402b775a
Gabor Majoros <gabormajo@yahoo.com>
2019-05-31 14:08:54 +0200

    implemented getSystems at ServiceRegistryController

--- serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/ServiceRegistryController.java
+++ serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/ServiceRegistryController.java
@@ -6,9 +6,11 @@ import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,7 @@ import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.common.database.entity.System;
 import eu.arrowhead.common.database.service.ServiceRegistryDBService;
 import eu.arrowhead.common.dto.DTOConverter;
+import eu.arrowhead.common.dto.SystemListResponseDTO;
 import eu.arrowhead.common.dto.SystemResponseDTO;
 import eu.arrowhead.common.exception.BadPayloadException;
 import eu.arrowhead.common.exception.DataNotFoundException;
@@ -36,6 +39,10 @@ public class ServiceRegistryController {
 	private static final String GET_SYSTEM_BY_ID_HTTP_417_MESSAGE = "Not a valid System id";
 	private static final String SYSTEM_BY_ID_PATH_VARIABLE = "id";
 	private static final String SYSTEM_BY_ID_URI = "/mgmt/system/{" + SYSTEM_BY_ID_PATH_VARIABLE + "}";
+	private static final String SYSTEMS_URI = "/mgmt/systems";
+	private static final String GET_SYSTEMS_HTTP_200_MESSAGE = "Systems returned";
+	private static final String GET_SYSTEMS_HTTP_400_MESSAGE = "";
+	private static final String GET_SYSTEMS_HTTP_417_MESSAGE = "Not valid request parameters";
 	
 	private final Logger logger = LogManager.getLogger(ServiceRegistryController.class);
 
@@ -87,6 +94,50 @@ public class ServiceRegistryController {
 	}
 	
 	//-------------------------------------------------------------------------------------------------
-	//TODO
-	//implement List<SystemResponseDTO> getSystems(int page, int itemPerPage){...}
-}
+
+	@ApiOperation(value = "Return systems by request parameters", response = SystemResponseDTO.class)
+	@ApiResponses (value = {
+			@ApiResponse(code = HttpStatus.SC_OK, message = GET_SYSTEMS_HTTP_200_MESSAGE),
+			@ApiResponse(code = HttpStatus.SC_BAD_REQUEST, message = GET_SYSTEMS_HTTP_400_MESSAGE),
+			@ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = CommonConstants.SWAGGER_HTTP_401_MESSAGE),
+			@ApiResponse(code = HttpStatus.SC_EXPECTATION_FAILED, message = GET_SYSTEMS_HTTP_417_MESSAGE),
+			@ApiResponse(code = HttpStatus.SC_INTERNAL_SERVER_ERROR, message = CommonConstants.SWAGGER_HTTP_500_MESSAGE)
+	})
+	@GetMapping(SYSTEMS_URI)
+	@ResponseBody public SystemListResponseDTO getSystems(
+			@RequestParam(name = CommonConstants.REQUEST_PARAM_PAGE, required = false) final Integer page,
+			@RequestParam(name = CommonConstants.REQUEST_PARAM_ITEM_PER_PAGE, required = false) final Integer size,
+			@RequestParam(name = CommonConstants.REQUEST_PARAM_DIRECTION, defaultValue = "ASC") final String direction,
+			@RequestParam(name = CommonConstants.REQUEST_PARAM_SORT_FIELD, defaultValue = CommonConstants.COMMON_FIELD_NAME_ID) final String sortField){		
+		
+		logger.debug("getSystems started ...");
+		
+		final int validatedPage;
+		final int validatedSize;
+		
+		if ( page == null && size==null ) {
+			
+			validatedPage = -1;
+			validatedSize = -1;
+		
+		} else {
+			if ( page == null || size==null ) {
+				
+				throw new BadPayloadException(" Invalid paraameters", HttpStatus.SC_EXPECTATION_FAILED, CommonConstants.SERVICEREGISTRY_URI + SYSTEMS_URI);
+			
+			} else {
+				validatedPage = page;
+				validatedSize = size;
+			}			
+		}
+		
+		try {			
+			
+			return DTOConverter.convertSystemEntryListToSystemListResponseDTO(serviceRegistryDBService.getSystemEntries(validatedPage, validatedSize, direction, sortField));	
+		
+		} catch ( final NoSuchElementException e) {
+			throw new DataNotFoundException("Not valid request parameters." , HttpStatus.SC_BAD_REQUEST, CommonConstants.SERVICEREGISTRY_URI + SYSTEMS_URI, e);
+		}
+		
+	}
+}	
