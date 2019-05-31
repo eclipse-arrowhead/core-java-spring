27d2fa70cecbe844d0fd1e5ee535ea354d72ad60
Gabor Majoros <gabormajo@yahoo.com>
2019-05-31 12:08:06 +0200

    implemented convertSystemEntryListToSystemListResponseDTO at DTOConverter

--- core-common/src/main/java/eu/arrowhead/common/dto/DTOConverter.java
+++ core-common/src/main/java/eu/arrowhead/common/dto/DTOConverter.java
@@ -1,6 +1,11 @@
 package eu.arrowhead.common.dto;
 
 import eu.arrowhead.common.database.entity.System;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.util.Assert;
 
 public class DTOConverter {
@@ -16,12 +21,42 @@ public class DTOConverter {
 		return new SystemResponseDTO(system.getSystemName(), system.getAddress(), system.getPort(), system.getAuthenticationInfo());		
 	}
 	
+	//-------------------------------------------------------------------------------------------------
+	public static SystemListResponseDTO convertSystemEntryListToSystemListResponseDTO(final Page<System> systemEntryList) {
+		Assert.notNull(systemEntryList, "systemEntryList is null");
+		
+		final long totalNumberOfSystems = systemEntryList.getTotalElements();		
+		
+		final SystemListResponseDTO systemListResponseDTO = new SystemListResponseDTO();
+		
+		systemListResponseDTO.setTotalNumberOfSystems(totalNumberOfSystems);
+		systemListResponseDTO.setSystemResponeDTOList(sytemEntryListToSystemResponeDTOList(systemEntryList.getContent()));
+		
+		return systemListResponseDTO;
+		
+	}
+	
 	//=================================================================================================
 	// assistant methods
 	
 	//-------------------------------------------------------------------------------------------------
+	
 	private DTOConverter() {
 		throw new UnsupportedOperationException();
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	
+	private static List<SystemResponseDTO> sytemEntryListToSystemResponeDTOList(final List<System> systemList) {
+		
+		final List<SystemResponseDTO> systemResponseDTOs = new ArrayList<SystemResponseDTO>();
+		
+		for (System system : systemList) {
+			systemResponseDTOs.add(convertSystemToSystemResponseDTO(system));
+		}
+		
+		return systemResponseDTOs;
+		
 	}
 	
 }
