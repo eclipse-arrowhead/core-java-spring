2699c4764794b64ce5327a5221a601afb02219ba
Gabor Majoros <gabormajo@yahoo.com>
2019-06-03 16:03:10 +0200

    removed unused imports from ServiceRegistryDBService

--- core-common/src/main/java/eu/arrowhead/common/database/service/ServiceRegistryDBService.java
+++ core-common/src/main/java/eu/arrowhead/common/database/service/ServiceRegistryDBService.java
@@ -1,11 +1,9 @@
 package eu.arrowhead.common.database.service;
 
-import java.time.ZonedDateTime;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
