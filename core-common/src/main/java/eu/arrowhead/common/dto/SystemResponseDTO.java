9e5b61efda669764fa54299cb18bbc51d4eebe29
Gabor Majoros <gabormajo@yahoo.com>
2019-06-04 13:38:12 +0200

    adding cration and update time to SystemResponseDTO

--- core-common/src/main/java/eu/arrowhead/common/dto/SystemResponseDTO.java
+++ core-common/src/main/java/eu/arrowhead/common/dto/SystemResponseDTO.java
@@ -14,10 +14,14 @@ public class SystemResponseDTO implements Serializable {
 	private String address;
 	private int port;
 	private String authenticationInfo;
+	private String createdAt;
+	private String updatedAt;
 	
 	//=================================================================================================
 	// methods
 	
+
+
 	//-------------------------------------------------------------------------------------------------
 	public SystemResponseDTO() {}
 	
@@ -37,6 +41,8 @@ public class SystemResponseDTO implements Serializable {
 	public String getAddress() { return address; }
 	public int getPort() { return port; }
 	public String getAuthenticationInfo() {	return authenticationInfo; }
+	public String getUpdatedAt() { return updatedAt; }
+	public String getCreatedAt() { return createdAt; }
 	
 	//-------------------------------------------------------------------------------------------------
 	public void setSytemId(long sytemId) { this.sytemId = sytemId; }
@@ -44,4 +50,6 @@ public class SystemResponseDTO implements Serializable {
 	public void setAddress(final String address) { this.address = address; }
 	public void setPort(final int port) { this.port = port; }
 	public void setAuthenticationInfo(final String authenticationInfo) { this.authenticationInfo = authenticationInfo; }
+	public void setCreatedAt(String createdAt) { this.createdAt = createdAt; }
+	public void setUpdatedAt(String updatedAt) { this.updatedAt = updatedAt; }
 }
