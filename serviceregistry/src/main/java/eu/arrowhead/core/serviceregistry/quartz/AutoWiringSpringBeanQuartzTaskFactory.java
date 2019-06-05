bb972ffd5c15bcdf8e37e810cc4e0051699bedec
Gabor Majoros <gabormajo@yahoo.com>
2019-06-05 14:08:28 +0200

    Refractored

--- serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/quartz/AutoWiringSpringBeanQuartzTaskFactory.java
+++ serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/quartz/AutoWiringSpringBeanQuartzTaskFactory.java
@@ -1,7 +1,6 @@
 package eu.arrowhead.core.serviceregistry.quartz;
 
 import org.quartz.spi.TriggerFiredBundle;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
