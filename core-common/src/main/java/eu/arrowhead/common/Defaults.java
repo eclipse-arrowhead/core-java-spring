202206bb621a05af0a10fcd3071106732489c8f5
rbocsi <rbocsi@aitia.ai>
2019-06-11 13:58:00 +0200

    service interface name verifier component

--- core-common/src/main/java/eu/arrowhead/common/Defaults.java
+++ core-common/src/main/java/eu/arrowhead/common/Defaults.java
@@ -10,16 +10,17 @@ public class Defaults {
 	public static final int VARCHAR_EXTENDED = 2047;
 	
 	public static final boolean DEFAULT_SSL_SERVER_ENABLED = true;
+	public static final boolean DEFAULT_DISABLE_HOSTNAME_VERIFIER = false;
+	public static final boolean DEFAULT_LOG_ALL_REQUEST_AND_RESPONSE = false;
 	public static final String DEFAULT_SERVICE_REGISTRY_ADDRESS = CommonConstants.LOCALHOST;
 	public static final int DEFAULT_SERVICE_REGISTRY_PORT = 8443;
+
 	public static final boolean DEFAULT_SERVICE_REGISTRY_PING_SCHEDULED = false;
 	public static final int DEFAULT_SERVICE_REGISTRY_PING_INTERVAL_MINUTES = 60;
 	public static final int DEFAULT_SERVICE_REGISTRY_PING_TIMEOUT_MILISECONDS = 5000;
 	public static final boolean DEFAULT_SERVICE_REGISTRY_TTL_SCHEDULED = false;
 	public static final int DEFAULT_SERVICE_REGISTRY_TTL_INTERVAL_MINUTES = 13;
-	public static final boolean DEFAULT_DISABLE_HOSTNAME_VERIFIER = false;
-	
-	public static final boolean DEFAULT_LOG_ALL_REQUEST_AND_RESPONSE = false;
+	public static final boolean DEFAULT_SERVICE_REGISTRY_USE_STRICT_SERVICE_INTF_NAME_VERIFIER = true;
 	
 	// HTTP client defaults
 	public static final int DEFAULT_CONNECTION_TIMEOUT = 30000;
