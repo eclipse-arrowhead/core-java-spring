2324dbcebb291b58ee8269bdce9320a99c33f6b7
borditamas <borditamas@gmail.com>
2019-06-12 10:55:40 +0200

    Manual fixing of merge issue

--- serviceregistry/src/test/java/eu/arrowhead/common/filter/PayloadSizeFilterTest.java
+++ serviceregistry/src/test/java/eu/arrowhead/common/filter/PayloadSizeFilterTest.java
@@ -21,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.core.serviceregistry.ServiceRegistryMain;
 import eu.arrowhead.core.serviceregistry.security.SRAccessControlFilter;
 
