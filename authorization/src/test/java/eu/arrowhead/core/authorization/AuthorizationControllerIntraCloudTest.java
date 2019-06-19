a1f31ab4389f832191bac18deef644d8b63adacb
borditamas <borditamas@gmail.com>
2019-06-19 15:46:01 +0200

    getIntraCloudAuthorizations controller and db tests

---
+++ authorization/src/test/java/eu/arrowhead/core/authorization/AuthorizationControllerIntraCloudTest.java
@@ -0,0 +1,150 @@
+package eu.arrowhead.core.authorization;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.database.entity.IntraCloudAuthorization;
+import eu.arrowhead.common.database.entity.ServiceDefinition;
+import eu.arrowhead.common.database.entity.System;
+import eu.arrowhead.common.dto.DTOConverter;
+import eu.arrowhead.common.dto.IntraCloudAuthorizationListResponseDTO;
+import eu.arrowhead.core.authorization.database.service.AuthorizationDBService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AuthorizationMain.class)
+@ContextConfiguration(classes = AuthorizationDBServiceTestContext.class)
+public class AuthorizationControllerIntraCloudTest {
+	
+	//=================================================================================================
+	// members
+	
+	private static final String INTRA_CLOUD_AUTHORIZATION_MGMT_URI = "/authorization/mgmt/intracloud";
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@MockBean(name = "mockAuthorizationDBService")
+	private AuthorizationDBService authorizationDBService;
+	
+	//-------------------------------------------------------------------------------------------------
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	// Tests of getIntraCloudAuthorizations
+	
+	@Test
+	public void testGetIntraCloudAuthorizationsWithoutParameters() throws Exception {
+		final int numOfEntries = 4;
+		final IntraCloudAuthorizationListResponseDTO dto = DTOConverter.convertIntraCloudAuthorizationListToIntraCloudAuthorizationListResponseDTO(createPageForMockingAuthorizationDBService(numOfEntries));
+		when(authorizationDBService.getIntraCloudAuthorizationEntriesResponse(anyInt(), anyInt(), any(), any())).thenReturn(dto);
+		
+		final MvcResult response = this.mockMvc.perform(get(INTRA_CLOUD_AUTHORIZATION_MGMT_URI)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		final IntraCloudAuthorizationListResponseDTO responseBody = objectMapper.readValue(response.getResponse().getContentAsByteArray(), IntraCloudAuthorizationListResponseDTO.class);
+		assertEquals(numOfEntries, responseBody.getData().size());
+		assertEquals(numOfEntries, responseBody.getCount());
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testGetIntraCloudAuthorizationsWithPageAndSizeParameter() throws Exception {
+		final int numOfEntries = 4;
+		final IntraCloudAuthorizationListResponseDTO dto = DTOConverter.convertIntraCloudAuthorizationListToIntraCloudAuthorizationListResponseDTO(createPageForMockingAuthorizationDBService(numOfEntries));
+		when(authorizationDBService.getIntraCloudAuthorizationEntriesResponse(anyInt(), anyInt(), any(), any())).thenReturn(dto);
+		
+		final MvcResult response = this.mockMvc.perform(get(INTRA_CLOUD_AUTHORIZATION_MGMT_URI)
+				.param(CommonConstants.REQUEST_PARAM_PAGE, "0")
+				.param(CommonConstants.REQUEST_PARAM_ITEM_PER_PAGE, String.valueOf(numOfEntries))
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		final IntraCloudAuthorizationListResponseDTO responseBody = objectMapper.readValue(response.getResponse().getContentAsByteArray(), IntraCloudAuthorizationListResponseDTO.class);
+		assertEquals(numOfEntries, responseBody.getData().size());
+		assertEquals(numOfEntries, responseBody.getCount());
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testGetIntraCloudAuthorizationsWithNullPageButDefinedSizeParameter() throws Exception {
+		this.mockMvc.perform(get(INTRA_CLOUD_AUTHORIZATION_MGMT_URI)
+				.param(CommonConstants.REQUEST_PARAM_ITEM_PER_PAGE, String.valueOf(5))
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest());
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testGetIntraCloudAuthorizationsWithDefinedPageButNullSizeParameter() throws Exception {
+		this.mockMvc.perform(get(INTRA_CLOUD_AUTHORIZATION_MGMT_URI)
+				.param(CommonConstants.REQUEST_PARAM_PAGE, "0")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest());
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testGetIntraCloudAuthorizationsWithInvalidSortDirectionFlagParameter() throws Exception {
+		this.mockMvc.perform(get(INTRA_CLOUD_AUTHORIZATION_MGMT_URI)
+				.param(CommonConstants.REQUEST_PARAM_DIRECTION, "invalid")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest());
+	}
+	
+	//=================================================================================================
+	// assistant methods
+
+	//-------------------------------------------------------------------------------------------------
+	private Page<IntraCloudAuthorization> createPageForMockingAuthorizationDBService(final int numberOfRequestedEntry) {
+		final List<IntraCloudAuthorization> entries = new ArrayList<>(numberOfRequestedEntry);
+		final ServiceDefinition serviceDefinition = new ServiceDefinition("testService");
+		for (int i = 1; i <= numberOfRequestedEntry; i++ ) {
+			final System consumer = new System("Consumer" + i, i + "." + i +"." + i + "." + i, i * 1000, null);
+			consumer.setId(i);
+			final System provider = new System("Provider" + i, i + "." + i +"." + i + "." + i, i * 1000, null);
+			provider.setId(i);
+			final IntraCloudAuthorization entry = new IntraCloudAuthorization(consumer, provider, serviceDefinition);
+			entry.setId(i);
+			entries.add(entry);
+		}
+		return new PageImpl<IntraCloudAuthorization>(entries);
+	}
+	
+}
---
+++ authorization/src/test/java/eu/arrowhead/core/authorization/database/service/AuthorizationDBServiceIntraCloudTest.java
@@ -0,0 +1,75 @@
+package eu.arrowhead.core.authorization.database.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.database.entity.IntraCloudAuthorization;
+import eu.arrowhead.common.database.entity.ServiceDefinition;
+import eu.arrowhead.common.database.entity.System;
+import eu.arrowhead.common.database.repository.IntraCloudAuthorizationRepository;
+import eu.arrowhead.common.exception.InvalidParameterException;
+
+@RunWith(SpringRunner.class)
+public class AuthorizationDBServiceIntraCloudTest {
+	
+	//=================================================================================================
+	// members
+	@InjectMocks
+	private AuthorizationDBService authorizationDBService;
+	
+	@Mock
+	private IntraCloudAuthorizationRepository intraCloudAuthorizationRepository;
+	
+	//=================================================================================================
+	// methods
+	
+	//-------------------------------------------------------------------------------------------------
+	//Tests of getIntraCloudAuthorizationEntries
+	
+	@Test
+	public void testGetIntraCloudAuthorizationEntriesCallDB() {
+		final int numOfEntries = 3;
+		when(intraCloudAuthorizationRepository.findAll(any(PageRequest.class))).thenReturn(createPageForMockingIntraCloudAuthorizationRepository(numOfEntries));
+		assertEquals(numOfEntries, authorizationDBService.getIntraCloudAuthorizationEntries(0, 10, Direction.ASC, CommonConstants.COMMON_FIELD_NAME_ID).getNumberOfElements());
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test(expected = InvalidParameterException.class)
+	public void testGetIntraCloudAuthorizationEntriesWithNotValidSortField() {
+		authorizationDBService.getIntraCloudAuthorizationEntries(0, 10, Direction.ASC, "notValid");
+	}
+	
+	//=================================================================================================
+	// assistant methods
+	
+	//-------------------------------------------------------------------------------------------------
+	private Page<IntraCloudAuthorization> createPageForMockingIntraCloudAuthorizationRepository(final int numberOfRequestedEntry) {
+		final List<IntraCloudAuthorization> entries = new ArrayList<>(numberOfRequestedEntry);
+		final ServiceDefinition serviceDefinition = new ServiceDefinition("testService");
+		for (int i = 1; i <= numberOfRequestedEntry; i++ ) {
+			final System consumer = new System("Consumer" + i, i + "." + i +"." + i + "." + i, i * 1000, null);
+			consumer.setId(i);
+			final System provider = new System("Provider" + i, i + "." + i +"." + i + "." + i, i * 1000, null);
+			provider.setId(i);
+			final IntraCloudAuthorization entry = new IntraCloudAuthorization(consumer, provider, serviceDefinition);
+			entry.setId(i);
+			entries.add(entry);
+		}
+		return new PageImpl<IntraCloudAuthorization>(entries);
+	}
+}
