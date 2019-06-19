2a9863c01101ee6d58c767d594353d1d689c3ff8
rbocsi <rbocsi@aitia.ai>
2019-06-19 16:16:59 +0200

    getOwnCloud database method + tests

--- core-common/src/main/java/eu/arrowhead/common/database/repository/CloudRepository.java
+++ core-common/src/main/java/eu/arrowhead/common/database/repository/CloudRepository.java
@@ -1,11 +1,20 @@
 package eu.arrowhead.common.database.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.google.common.base.Optional;
 
 import eu.arrowhead.common.database.entity.Cloud;
 
 @Repository
-public interface CloudRepository extends JpaRepository<Cloud, Long> {
+public interface CloudRepository extends JpaRepository<Cloud,Long> {
 
+	//=================================================================================================
+	// methods
+	
+	//-------------------------------------------------------------------------------------------------
+	public List<Cloud> findByOwnCloudAndSecure(final boolean ownCloud, final boolean secure);
 }
---
+++ core-common/src/main/java/eu/arrowhead/common/database/service/CommonDBService.java
@@ -0,0 +1,51 @@
+package eu.arrowhead.common.database.service;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.base.Optional;
+
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.database.entity.Cloud;
+import eu.arrowhead.common.database.repository.CloudRepository;
+import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.common.exception.DataNotFoundException;
+
+@Service
+public class CommonDBService {
+
+	//=================================================================================================
+	// members
+	
+	@Autowired
+	private CloudRepository cloudRepository;
+
+	private final Logger logger = LogManager.getLogger(CommonDBService.class);
+	
+	//=================================================================================================
+	// methods
+	
+	//-------------------------------------------------------------------------------------------------
+	public Cloud getOwnCloud(final boolean isSecure) {
+		logger.debug("getOwnCloud started...");
+		try {
+			final List<Cloud> cloudList = cloudRepository.findByOwnCloudAndSecure(true, isSecure);
+			if (cloudList.isEmpty()) {
+				throw new DataNotFoundException("Could not find own cloud information in the database");
+			} else if (cloudList.size() > 1) {
+				throw new ArrowheadException("More than one cloud is marked as own in " + (isSecure ? "SECURE" : "INSECURE") + " mode.");
+			}
+			
+			return cloudList.get(0);
+		} catch (final ArrowheadException ex) {
+			throw ex;
+		} catch (final Exception ex) {
+			logger.debug(ex.getMessage(), ex);
+			throw new ArrowheadException(CommonConstants.DATABASE_OPERATION_EXCEPTION_MSG);
+		}		
+	}
+}
