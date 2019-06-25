42c30be288860dafdd60413787e24851ea158177
rbocsi <rbocsi@aitia.ai>
2019-06-25 17:21:41 +0200

    fixed TokenSecurityFilter + tests

--- core-common/src/main/java/eu/arrowhead/common/token/TokenSecurityFilter.java
+++ core-common/src/main/java/eu/arrowhead/common/token/TokenSecurityFilter.java
@@ -16,6 +16,7 @@ import org.springframework.util.Assert;
 
 import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.exception.ArrowheadException;
 import eu.arrowhead.common.exception.AuthException;
 import eu.arrowhead.common.filter.ArrowheadFilter;
 import eu.arrowhead.common.token.TokenUtilities.TokenInfo;
@@ -32,35 +33,29 @@ public abstract class TokenSecurityFilter extends ArrowheadFilter {
 	// methods
 	
 	//-------------------------------------------------------------------------------------------------
-	public TokenSecurityFilter(final PrivateKey myPrivateKey, final PublicKey authorizationPublicKey) {
-		super();
-		Assert.notNull(myPrivateKey, "My private key is null.");
-		Assert.notNull(authorizationPublicKey, "Authorization public key is null.");
-		
-		this.myPrivateKey = myPrivateKey;
-		this.authorizationPublicKey = authorizationPublicKey;
-	}
-	
-	//-------------------------------------------------------------------------------------------------
 	@Override
 	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
 		if (request instanceof HttpServletRequest) {
 			log.debug("Checking access in TokenSecurityFilter...");
-			final HttpServletRequest httpRequest = (HttpServletRequest) request;
-			final String requestTarget = Utilities.stripEndSlash(httpRequest.getRequestURL().toString());
-			final String clientCN = getCertificateCNFromRequest(httpRequest);
-			if (clientCN == null) {
-				log.error("Unauthorized access: {}", requestTarget);
-				throw new AuthException("Unauthorized access: " + requestTarget);
+			try {
+				final HttpServletRequest httpRequest = (HttpServletRequest) request;
+				final String requestTarget = Utilities.stripEndSlash(httpRequest.getRequestURL().toString());
+				final String clientCN = getCertificateCNFromRequest(httpRequest);
+				if (clientCN == null) {
+					log.error("Unauthorized access: {}", requestTarget);
+					throw new AuthException("Unauthorized access: " + requestTarget);
+				}
+				
+				final String token = httpRequest.getParameter(CommonConstants.REQUEST_PARAM_TOKEN);
+				if (Utilities.isEmpty(token)) {
+					log.error("Unauthorized access: {}, no token is specified", requestTarget);
+					throw new AuthException("Unauthorized access: " + requestTarget + ", no token is specified");
+				}
+				
+				checkToken(clientCN, token, requestTarget);
+			} catch (final ArrowheadException ex) {
+				handleException(ex, response);
 			}
-
-			final String token = httpRequest.getParameter(CommonConstants.REQUEST_PARAM_TOKEN);
-			if (Utilities.isEmpty(token)) {
-				log.error("Unauthorized access: {}, no token is specified", requestTarget);
-				throw new AuthException("Unauthorized access: " + requestTarget + ", no token is specified");
-			}
-			
-			checkToken(clientCN, token, requestTarget);
 		}
 		
 		chain.doFilter(request, response);
@@ -70,9 +65,20 @@ public abstract class TokenSecurityFilter extends ArrowheadFilter {
 	// assistant methods
 	
 	//-------------------------------------------------------------------------------------------------
+	protected TokenSecurityFilter(final PrivateKey myPrivateKey, final PublicKey authorizationPublicKey) {
+		super();
+		Assert.notNull(myPrivateKey, "My private key is null.");
+		Assert.notNull(authorizationPublicKey, "Authorization public key is null.");
+		
+		this.myPrivateKey = myPrivateKey;
+		this.authorizationPublicKey = authorizationPublicKey;
+	}
+	
+	//-------------------------------------------------------------------------------------------------
 	protected void checkToken(final String clientCN, final String token, final String requestTarget) {
+		final String clientName = clientCN.split("\\.")[0];
 		final TokenInfo tokenInfo = TokenUtilities.validateTokenAndExtractTokenInfo(token, authorizationPublicKey, myPrivateKey); // expiration (if set) is already checked in this method
-		if (!clientCN.equalsIgnoreCase(tokenInfo.getConsumerName())) {
+		if (!clientName.equalsIgnoreCase(tokenInfo.getConsumerName())) {
 			log.error("Client CN ({}) and token information ({}) is mismatched at: {}", clientCN, tokenInfo.getConsumerName(), requestTarget);
 			throw new AuthException("Unauthorized accesss: " + requestTarget + ", invalid token.");
 		}
