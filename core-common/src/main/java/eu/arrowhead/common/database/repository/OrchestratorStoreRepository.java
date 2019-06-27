2605a008c9f679340a8fcf71ed96711fbd56beeb
MaGaMeGa <gabormajo@yahoo.com>
2019-06-27 22:24:58 +0200

    removing method from OrchestratorStoreRepository

--- core-common/src/main/java/eu/arrowhead/common/database/repository/OrchestratorStoreRepository.java
+++ core-common/src/main/java/eu/arrowhead/common/database/repository/OrchestratorStoreRepository.java
@@ -1,7 +1,5 @@
 package eu.arrowhead.common.database.repository;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Repository;
 
 import eu.arrowhead.common.database.entity.OrchestratorStore;
@@ -9,5 +7,4 @@ import eu.arrowhead.common.database.entity.OrchestratorStore;
 @Repository
 public interface OrchestratorStoreRepository extends RefreshableRepository<OrchestratorStore, Long> {
 
-	Optional<OrchestratorStore> findById(Long id);
 }
