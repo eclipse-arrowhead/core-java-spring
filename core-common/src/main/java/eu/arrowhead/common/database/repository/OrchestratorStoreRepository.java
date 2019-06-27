15680feffbfac26517d24b0cfeb7c89933ecd700
MaGaMeGa <gabormajo@yahoo.com>
2019-06-27 20:43:08 +0200

    adding annotation to OrchestratorStoreRepository

--- core-common/src/main/java/eu/arrowhead/common/database/repository/OrchestratorStoreRepository.java
+++ core-common/src/main/java/eu/arrowhead/common/database/repository/OrchestratorStoreRepository.java
@@ -2,8 +2,11 @@ package eu.arrowhead.common.database.repository;
 
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import eu.arrowhead.common.database.entity.OrchestratorStore;
 
+@Repository
 public interface OrchestratorStoreRepository extends RefreshableRepository<OrchestratorStore, Long> {
 
 	Optional<OrchestratorStore> findById(Long id);
