928a2d0768a7ce9338430e9c73ca707707df3fc4
Gabor Majoros <gabormajo@yahoo.com>
2019-07-05 09:10:57 +0200

    removed unused import

--- orchestrator/src/main/java/eu/arrowhead/core/orchestrator/database/service/OrchestratorStoreDBService.java
+++ orchestrator/src/main/java/eu/arrowhead/core/orchestrator/database/service/OrchestratorStoreDBService.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
