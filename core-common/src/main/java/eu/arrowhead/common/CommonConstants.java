8ebbf27321fa8e3e2d8df72b70ed0a9e5fda567f
borditamas <borditamas@gmail.com>
2019-07-24 10:28:36 +0200

    gk services to register into SRE

--- core-common/src/main/java/eu/arrowhead/common/CommonConstants.java
+++ core-common/src/main/java/eu/arrowhead/common/CommonConstants.java
@@ -32,6 +32,8 @@ public class CommonConstants {
 	public static final String CORE_SERVICE_AUTH_PUBLIC_KEY = "public-key";
 	public static final String CORE_SERVICE_AUTH_CONTROL_INTRA = "authorization-control-intra";
 	public static final String CORE_SERVICE_AUTH_CONTROL_INTER = "authorization-control-inter";
+	public static final String CORE_SERVICE_GATEKEEPER_GSD = "global-service-discovery";
+	public static final String CORE_SERVICE_GATEKEEPER_ICN = "inter-cloud-negotiations";
 	
 	public static final String COMMON_FIELD_NAME_ID = "id";
 	
@@ -78,6 +80,8 @@ public class CommonConstants {
 	public static final String OP_AUTH_INTER_CHECK_URI = "/intercloud/check";
 	
 	public static final String GATEKEEPER_URI = "/gatekeeper";
+	public static final String OP_GATEKEEPER_GSD_SERVICE = "/init_gsd";
+	public static final String OP_GATEKEEPER_ICN_SERVICE = "/init_icn";
 
 	public static final String SWAGGER_COMMON_PACKAGE = "eu.arrowhead.common.swagger";
 	public static final String SWAGGER_UI_URI = "/swagger-ui.html";
