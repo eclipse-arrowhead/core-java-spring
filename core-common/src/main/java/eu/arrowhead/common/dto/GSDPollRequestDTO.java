329986c1447202757084e34001e3188649be580e
borditamas <borditamas@gmail.com>
2019-08-08 14:45:27 +0200

    GSDPollRequest/ResponseDTO impls

--- core-common/src/main/java/eu/arrowhead/common/dto/GSDPollRequestDTO.java
+++ core-common/src/main/java/eu/arrowhead/common/dto/GSDPollRequestDTO.java
@@ -4,16 +4,30 @@ import java.io.Serializable;
 
 public class GSDPollRequestDTO implements Serializable {
 
-	//TODO: implement this class
-	//TODO: need change in ActiveMQGatekeeperRelayTest if you changed the members
+	//=================================================================================================
+	// members
 	
-	private String requestedServiceDefinition;
-
-	public String getRequestedServiceDefinition() {
-		return requestedServiceDefinition;
+	private static final long serialVersionUID = 3327546226003685713L;
+	
+	private ServiceQueryFormDTO requestedService;
+	private CloudRequestDTO requesterCloud;	
+	
+	//=================================================================================================
+	// methods
+	
+	//-------------------------------------------------------------------------------------------------
+	public GSDPollRequestDTO() {} 	
+	
+	public GSDPollRequestDTO(final ServiceQueryFormDTO requestedService, final CloudRequestDTO requesterCloud) {
+		this.requestedService = requestedService;
+		this.requesterCloud = requesterCloud;
 	}
 
-	public void setRequestedServiceDefinition(String requestedServiceDefinition) {
-		this.requestedServiceDefinition = requestedServiceDefinition;
-	}
+	//-------------------------------------------------------------------------------------------------	
+	public ServiceQueryFormDTO getRequestedService() { return requestedService; }
+	public CloudRequestDTO getRequesterCloud() { return requesterCloud; }
+	
+	//-------------------------------------------------------------------------------------------------	
+	public void setRequestedService(final ServiceQueryFormDTO requestedService) { this.requestedService = requestedService; }
+	public void setRequesterCloud(final CloudRequestDTO requesterCloud) { this.requesterCloud = requesterCloud; }	
 }
