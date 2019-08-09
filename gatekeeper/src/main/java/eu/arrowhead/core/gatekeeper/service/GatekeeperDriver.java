d828587eabcdd7d810632b609aa89062b03aafdd
rbocsi <rbocsi@aitia.ai>
2019-08-09 14:20:45 +0200

    GatekeeperDriver initializes relayClient

--- gatekeeper/src/main/java/eu/arrowhead/core/gatekeeper/service/GatekeeperDriver.java
+++ gatekeeper/src/main/java/eu/arrowhead/core/gatekeeper/service/GatekeeperDriver.java
@@ -1,18 +1,27 @@
 package eu.arrowhead.core.gatekeeper.service;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.common.database.entity.Cloud;
 import eu.arrowhead.common.database.entity.Relay;
+import eu.arrowhead.common.dto.ICNProposalRequestDTO;
+import eu.arrowhead.common.dto.ICNProposalResponseDTO;
+import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.core.gatekeeper.relay.GatekeeperRelayClient;
+import eu.arrowhead.core.gatekeeper.relay.RelayClientFactory;
 import eu.arrowhead.core.gatekeeper.service.matchmaking.GatekeeperMatchmakingAlgorithm;
 import eu.arrowhead.core.gatekeeper.service.matchmaking.GatekeeperMatchmakingParameters;
 
@@ -25,12 +34,47 @@ public class GatekeeperDriver {
 	@Resource(name = CommonConstants.GATEKEEPER_MATCHMAKER)
 	private GatekeeperMatchmakingAlgorithm gatekeeperMatchmaker;
 	
+	@Resource(name = CommonConstants.ARROWHEAD_CONTEXT)
+	private Map<String,Object> arrowheadContext;
+	
+	@Value(CommonConstants.$HTTP_CLIENT_SOCKET_TIMEOUT_WD)
+	private long timeout;
+	
 	private final Logger logger = LogManager.getLogger(GatekeeperDriver.class);
+	
+	private GatekeeperRelayClient relayClient;
 	
 	//=================================================================================================
 	// methods
 	
-	//TODO
+	//-------------------------------------------------------------------------------------------------
+	@PostConstruct
+	public void init() {
+		if (!arrowheadContext.containsKey(CommonConstants.SERVER_COMMON_NAME)) {
+			throw new ArrowheadException("Server's certificate not found.");
+		}
+		final String serverCN = (String) arrowheadContext.get(CommonConstants.SERVER_COMMON_NAME);
+		
+		if (!arrowheadContext.containsKey(CommonConstants.SERVER_PUBLIC_KEY)) {
+			throw new ArrowheadException("Server's public key is not found.");
+		}
+		final PublicKey publicKey = (PublicKey) arrowheadContext.get(CommonConstants.SERVER_PUBLIC_KEY);
+		
+		if (!arrowheadContext.containsKey(CommonConstants.SERVER_PRIVATE_KEY)) {
+			throw new ArrowheadException("Server's private key is not found.");
+		}
+		final PrivateKey privateKey = (PrivateKey) arrowheadContext.get(CommonConstants.SERVER_PRIVATE_KEY);
+
+		relayClient = RelayClientFactory.createGatekeeperRelayClient(serverCN, publicKey, privateKey, timeout);
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	public ICNProposalResponseDTO sendICNProposal(final Cloud targetCloud, final ICNProposalRequestDTO request) {
+		logger.debug("sendICNProposal started...");
+		//TODO:
+		
+		return null;
+	}
 	
 	//=================================================================================================
 	// assistant methods
