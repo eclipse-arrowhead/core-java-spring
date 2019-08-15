2e3a0b365b9e28b737d8125ddd39dd0fdca91a3a
rbocsi <rbocsi@aitia.ai>
2019-08-15 13:15:01 +0200

    fixing certificate and shell script

--- scripts/start_coresystems.sh
+++ scripts/start_coresystems.sh
@@ -7,7 +7,7 @@ time_to_sleep=10s
 
 echo Starting Core Systems... Service initializations usually need around 20 seconds.
 
-cd ../serviceregistryl/target
+cd ../serviceregistry/target
 nohup java -jar $(find . -maxdepth 1 -name arrowhead-serviceregistry-\*.jar | sort | tail -n1) &> sout_sr.log &
 echo Service Registry started
 sleep ${time_to_sleep} #wait for the Service Registry to fully finish loading up
