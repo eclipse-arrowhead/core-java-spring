e56b89608bb86be3c96c51f726abbeb25b087dc7
Gabor Majoros <gabormajo@yahoo.com>
2019-08-23 16:20:43 +0200

    further implementations of RandomWeightedCloudMatchmaker tests

--- orchestrator/src/test/java/eu/arrowhead/core/orchestrator/matchmaking/RandomWeightedCloudMatchmakerTest.java
+++ orchestrator/src/test/java/eu/arrowhead/core/orchestrator/matchmaking/RandomWeightedCloudMatchmakerTest.java
@@ -92,7 +92,324 @@ public class RandomWeightedCloudMatchmakerTest {
 		
 		Assert.assertTrue( provider1 > provider2 );
 
+	}
 	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testDoMatchmakingMoreWeightResultsMoreMatchingOnlyIfItIsPreferred() {		
+		
+		final boolean onlyPreferred = false;
+		final CloudRequestDTO cloudRequestDTO = new CloudRequestDTO();
+		cloudRequestDTO.setOperator("operator2");
+		cloudRequestDTO.setName("cloudname2");
+		
+		final List<CloudRequestDTO> preferredClouds = List.of(cloudRequestDTO);
+
+		final CloudResponseDTO providerCloud1 = new CloudResponseDTO(
+				1L, 
+				"operator1", 
+				"cloudname1", 
+				false, 
+				true, 
+				false, 
+				null, 
+				null, 
+				null);
+		final int numOfProviders10 = 10;		
+		final GSDPollResponseDTO gsdPollResponseDTO1 = new GSDPollResponseDTO(
+				providerCloud1,
+				"requiredServiceDefinition",
+				null,
+				numOfProviders10,
+				null);
+		
+		final CloudResponseDTO providerCloud2 = new CloudResponseDTO(
+				1L, 
+				"operator2", 
+				"cloudname2", 
+				false, 
+				true, 
+				false, 
+				null, 
+				null, 
+				null);
+		final int numOfProviders1 = 1;		
+		final GSDPollResponseDTO gsdPollResponseDTO2 = new GSDPollResponseDTO(
+				providerCloud2,
+				"requiredServiceDefinition",
+				null,
+				numOfProviders1,
+				null);
+		final List<GSDPollResponseDTO> gsdPollResponseDTOList = List.of(gsdPollResponseDTO1, gsdPollResponseDTO2);
+		final int unsuccessfulRequests = 0;
+		final GSDQueryResultDTO gsdQueryResultDTO = new GSDQueryResultDTO(
+				gsdPollResponseDTOList, 
+				unsuccessfulRequests);
+		
+		final CloudMatchmakingParameters params = new CloudMatchmakingParameters(
+				gsdQueryResultDTO, 
+				preferredClouds, 
+				onlyPreferred);	
+		
+		int provider1 = 0;
+		int provider2 = 0;
+		for (int i = 0; i < 100; i++) {
+			
+			final CloudResponseDTO cloudResponseDTO = algorithm.doMatchmaking(params);
+			
+			if (cloudResponseDTO.getName().equalsIgnoreCase(providerCloud1.getName())) {
+				
+				++provider1;
+			}else {
+				++provider2;
+			}
+		}
+		
+		Assert.assertFalse( provider1 > provider2 );
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testDoMatchmakingOnlyPreferred() {		
+		
+		final boolean onlyPreferred = true;
+		final CloudRequestDTO cloudRequestDTO = new CloudRequestDTO();
+		cloudRequestDTO.setOperator("operator100");
+		cloudRequestDTO.setName("cloudname100");
+		
+		final List<CloudRequestDTO> preferredClouds = List.of(cloudRequestDTO);
+
+		final CloudResponseDTO providerCloud1 = new CloudResponseDTO(
+				1L, 
+				"operator1", 
+				"cloudname1", 
+				false, 
+				true, 
+				false, 
+				null, 
+				null, 
+				null);
+		final int numOfProviders10 = 10;		
+		final GSDPollResponseDTO gsdPollResponseDTO1 = new GSDPollResponseDTO(
+				providerCloud1,
+				"requiredServiceDefinition",
+				null,
+				numOfProviders10,
+				null);
+		
+		final CloudResponseDTO providerCloud2 = new CloudResponseDTO(
+				1L, 
+				"operator2", 
+				"cloudname2", 
+				false, 
+				true, 
+				false, 
+				null, 
+				null, 
+				null);
+		final int numOfProviders1 = 1;		
+		final GSDPollResponseDTO gsdPollResponseDTO2 = new GSDPollResponseDTO(
+				providerCloud2,
+				"requiredServiceDefinition",
+				null,
+				numOfProviders1,
+				null);
+		final List<GSDPollResponseDTO> gsdPollResponseDTOList = List.of(gsdPollResponseDTO1, gsdPollResponseDTO2);
+		final int unsuccessfulRequests = 0;
+		final GSDQueryResultDTO gsdQueryResultDTO = new GSDQueryResultDTO(
+				gsdPollResponseDTOList, 
+				unsuccessfulRequests);
+		
+		final CloudMatchmakingParameters params = new CloudMatchmakingParameters(
+				gsdQueryResultDTO, 
+				preferredClouds, 
+				onlyPreferred);	
+		
+		final CloudResponseDTO cloudResponseDTO = algorithm.doMatchmaking(params);
+		
+		Assert.assertTrue( cloudResponseDTO.getName() == null );
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testDoMatchmakingNotOnlyPreferred() {		
+		
+		final boolean onlyPreferred = false;
+		final CloudRequestDTO cloudRequestDTO = new CloudRequestDTO();
+		cloudRequestDTO.setOperator("operator100");
+		cloudRequestDTO.setName("cloudname100");
+		
+		final List<CloudRequestDTO> preferredClouds = List.of(cloudRequestDTO);
+
+		final CloudResponseDTO providerCloud1 = new CloudResponseDTO(
+				1L, 
+				"operator1", 
+				"cloudname1", 
+				false, 
+				true, 
+				false, 
+				null, 
+				null, 
+				null);
+		final int numOfProviders10 = 10;		
+		final GSDPollResponseDTO gsdPollResponseDTO1 = new GSDPollResponseDTO(
+				providerCloud1,
+				"requiredServiceDefinition",
+				null,
+				numOfProviders10,
+				null);
+		
+		final CloudResponseDTO providerCloud2 = new CloudResponseDTO(
+				1L, 
+				"operator2", 
+				"cloudname2", 
+				false, 
+				true, 
+				false, 
+				null, 
+				null, 
+				null);
+		final int numOfProviders1 = 1;		
+		final GSDPollResponseDTO gsdPollResponseDTO2 = new GSDPollResponseDTO(
+				providerCloud2,
+				"requiredServiceDefinition",
+				null,
+				numOfProviders1,
+				null);
+		final List<GSDPollResponseDTO> gsdPollResponseDTOList = List.of(gsdPollResponseDTO1, gsdPollResponseDTO2);
+		final int unsuccessfulRequests = 0;
+		final GSDQueryResultDTO gsdQueryResultDTO = new GSDQueryResultDTO(
+				gsdPollResponseDTOList, 
+				unsuccessfulRequests);
+		
+		final CloudMatchmakingParameters params = new CloudMatchmakingParameters(
+				gsdQueryResultDTO, 
+				preferredClouds, 
+				onlyPreferred);	
+		
+		final CloudResponseDTO cloudResponseDTO = algorithm.doMatchmaking(params);
+		
+		Assert.assertTrue( cloudResponseDTO.getName() != null );
+	}
+
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test(expected = IllegalArgumentException.class)
+	public void testDoMatchmakingNullParameter() {		
+		
+		algorithm.doMatchmaking( null );
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testDoMatchmakigNullGSDQueryResultDTO() {		
+		
+		final boolean onlyPreferred = false;
+		final CloudRequestDTO cloudRequestDTO = new CloudRequestDTO();
+		cloudRequestDTO.setOperator("operator100");
+		cloudRequestDTO.setName("cloudname100");
+		
+		final List<CloudRequestDTO> preferredClouds = List.of(cloudRequestDTO);
+		
+		final GSDQueryResultDTO gsdQueryResultDTO = null;
+		
+		final CloudMatchmakingParameters params = new CloudMatchmakingParameters(
+				gsdQueryResultDTO, 
+				preferredClouds, 
+				onlyPreferred);	
+		
+		final CloudResponseDTO cloudResponseDTO = algorithm.doMatchmaking(params);
+		
+		Assert.assertTrue( cloudResponseDTO.getName() == null );
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testDoMatchmakigEmptyGSDQueryResultDTO() {		
+		
+		final boolean onlyPreferred = false;
+		final CloudRequestDTO cloudRequestDTO = new CloudRequestDTO();
+		cloudRequestDTO.setOperator("operator100");
+		cloudRequestDTO.setName("cloudname100");
+		
+		final List<CloudRequestDTO> preferredClouds = List.of(cloudRequestDTO);
+		
+		final List<GSDPollResponseDTO> gsdPollResponseDTOList = List.of();
+		final int unsuccessfulRequests = 0;
+		final GSDQueryResultDTO gsdQueryResultDTO = new GSDQueryResultDTO(
+				gsdPollResponseDTOList, 
+				unsuccessfulRequests);
+		
+		final CloudMatchmakingParameters params = new CloudMatchmakingParameters(
+				gsdQueryResultDTO, 
+				preferredClouds, 
+				onlyPreferred);	
+		
+		final CloudResponseDTO cloudResponseDTO = algorithm.doMatchmaking(params);
+		
+		Assert.assertTrue( cloudResponseDTO.getName() == null );
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testDoMatchmakingProvidersWithZeroWeightWillNotBeIncludedInResponse() {		
+		
+		final boolean onlyPreferred = false;
+		final CloudRequestDTO cloudRequestDTO = new CloudRequestDTO();
+		cloudRequestDTO.setOperator("operator100");
+		cloudRequestDTO.setName("cloudname100");
+		
+		final List<CloudRequestDTO> preferredClouds = List.of(cloudRequestDTO);
+
+		final CloudResponseDTO providerCloud1 = new CloudResponseDTO(
+				1L, 
+				"operator1", 
+				"cloudname1", 
+				false, 
+				true, 
+				false, 
+				null, 
+				null, 
+				null);
+		final int numOfProviders10 = 0;		
+		final GSDPollResponseDTO gsdPollResponseDTO1 = new GSDPollResponseDTO(
+				providerCloud1,
+				"requiredServiceDefinition",
+				null,
+				numOfProviders10,
+				null);
+		
+		final CloudResponseDTO providerCloud2 = new CloudResponseDTO(
+				1L, 
+				"operator2", 
+				"cloudname2", 
+				false, 
+				true, 
+				false, 
+				null, 
+				null, 
+				null);
+		final int numOfProviders1 = 0;		
+		final GSDPollResponseDTO gsdPollResponseDTO2 = new GSDPollResponseDTO(
+				providerCloud2,
+				"requiredServiceDefinition",
+				null,
+				numOfProviders1,
+				null);
+		final List<GSDPollResponseDTO> gsdPollResponseDTOList = List.of(gsdPollResponseDTO1, gsdPollResponseDTO2);
+		final int unsuccessfulRequests = 0;
+		final GSDQueryResultDTO gsdQueryResultDTO = new GSDQueryResultDTO(
+				gsdPollResponseDTOList, 
+				unsuccessfulRequests);
+		
+		final CloudMatchmakingParameters params = new CloudMatchmakingParameters(
+				gsdQueryResultDTO, 
+				preferredClouds, 
+				onlyPreferred);	
+		
+		final CloudResponseDTO cloudResponseDTO = algorithm.doMatchmaking(params);
+		
+		Assert.assertTrue( cloudResponseDTO.getName() == null );
 	}
 
 }
