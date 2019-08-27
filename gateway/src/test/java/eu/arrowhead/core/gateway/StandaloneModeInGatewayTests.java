fd9814fc1fa967f1b7ceae7c51113192dc0c1336
borditamas <borditamas@gmail.com>
2019-08-27 17:23:34 +0200

    GatewayAccessControlFilter impl with tests

---
+++ gateway/src/test/java/eu/arrowhead/core/gateway/StandaloneModeInGatewayTests.java
@@ -0,0 +1,10 @@
+package eu.arrowhead.core.gateway;
+
+import org.springframework.stereotype.Component;
+
+import eu.arrowhead.common.testhelper.StandaloneModeInTests;
+
+@Component
+public class StandaloneModeInGatewayTests extends StandaloneModeInTests  {
+
+}
