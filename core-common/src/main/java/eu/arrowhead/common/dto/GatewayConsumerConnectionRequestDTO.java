c2e2db1a14340f2e25474dacd85d7de1da98cd91
rbocsi <rbocsi@aitia.ai>
2019-08-30 10:55:13 +0200

    dto for connectConsumer

--- core-common/src/main/java/eu/arrowhead/common/dto/GatewayConsumerConnectionRequestDTO.java
+++ core-common/src/main/java/eu/arrowhead/common/dto/GatewayConsumerConnectionRequestDTO.java
@@ -7,19 +7,18 @@ public class GatewayConsumerConnectionRequestDTO implements Serializable {
 	//=================================================================================================
 	// members
 	
-	private static final long serialVersionUID = 8982448541331553443L;
+	private static final long serialVersionUID = -4911124425438496148L;
 	
 	private RelayRequestDTO relay;
-	private String queueName;
-	private String controlQueueName;
+	private String queueId;
+	private String peerName;
+	private String providerGWPublicKey;
+
 	private SystemRequestDTO consumer;
 	private SystemRequestDTO provider;
 	private CloudRequestDTO consumerCloud;
 	private CloudRequestDTO providerCloud;
 	private String serviceDefinition;
-	private boolean isSecure;
-	private int timeout;
-	private String providerGWPublicKey;
 	
 	//=================================================================================================
 	// methods
@@ -28,44 +27,38 @@ public class GatewayConsumerConnectionRequestDTO implements Serializable {
 	public GatewayConsumerConnectionRequestDTO() {}
 
 	//-------------------------------------------------------------------------------------------------
-	public GatewayConsumerConnectionRequestDTO(final RelayRequestDTO relay, final String queueName, final String controlQueueName, final SystemRequestDTO consumer, final SystemRequestDTO provider, final CloudRequestDTO consumerCloud, 
-											   final CloudRequestDTO providerCloud, final String serviceDefinition, final boolean isSecure, final int timeout, final String providerGWPublicKey) {
+	public GatewayConsumerConnectionRequestDTO(final RelayRequestDTO relay, final String queueId, final String peerName, final String providerGWPublicKey, final SystemRequestDTO consumer,
+											   final SystemRequestDTO provider, final CloudRequestDTO consumerCloud, final CloudRequestDTO providerCloud, final String serviceDefinition) {
 		this.relay = relay;
-		this.queueName = queueName;
-		this.controlQueueName = controlQueueName;
+		this.queueId = queueId;
+		this.peerName = peerName;
+		this.providerGWPublicKey = providerGWPublicKey;
 		this.consumer = consumer;
 		this.provider = provider;
 		this.consumerCloud = consumerCloud;
 		this.providerCloud = providerCloud;
 		this.serviceDefinition = serviceDefinition;
-		this.isSecure = isSecure;
-		this.timeout = timeout;
-		this.providerGWPublicKey = providerGWPublicKey;
 	}
 
 	//-------------------------------------------------------------------------------------------------
 	public RelayRequestDTO getRelay() { return relay; }
-	public String getQueueName() { return queueName; }
-	public String getControlQueueName() { return controlQueueName; }
+	public String getQueueId() { return queueId; }
+	public String getPeerName() { return peerName; }
+	public String getProviderGWPublicKey() { return providerGWPublicKey; }
 	public SystemRequestDTO getConsumer() { return consumer; }
 	public SystemRequestDTO getProvider() { return provider; }
 	public CloudRequestDTO getConsumerCloud() { return consumerCloud; }
 	public CloudRequestDTO getProviderCloud() { return providerCloud; }
 	public String getServiceDefinition() { return serviceDefinition; }
-	public boolean isSecure() { return isSecure; }
-	public int getTimeout() { return timeout; }
-	public String getProviderGWPublicKey() { return providerGWPublicKey; }
 
 	//-------------------------------------------------------------------------------------------------
 	public void setRelay(final RelayRequestDTO relay) { this.relay = relay; }
-	public void setQueueName(final String queueName) { this.queueName = queueName; }
-	public void setControlQueueName(final String controlQueueName) { this.controlQueueName = controlQueueName; }
+	public void setQueueId(final String queueId) { this.queueId = queueId; }
+	public void setPeerName(final String peerName) { this.peerName = peerName; }
+	public void setProviderGWPublicKey(final String providerGWPublicKey) { this.providerGWPublicKey = providerGWPublicKey; }	
 	public void setConsumer(final SystemRequestDTO consumer) { this.consumer = consumer; }
 	public void setProvider(final SystemRequestDTO provider) { this.provider = provider; }
 	public void setConsumerCloud(final CloudRequestDTO consumerCloud) { this.consumerCloud = consumerCloud; }
 	public void setProviderCloud(final CloudRequestDTO providerCloud) { this.providerCloud = providerCloud; }
 	public void setServiceDefinition(final String serviceDefinition) { this.serviceDefinition = serviceDefinition; }
-	public void setSecure(final boolean isSecure) { this.isSecure = isSecure; }
-	public void setTimeout(final int timeout) { this.timeout = timeout; }
-	public void setProviderGWPublicKey(final String providerGWPublicKey) { this.providerGWPublicKey = providerGWPublicKey; }	
 }
