e493c5e4e4bc463aa33edb9f9fc0ea025d34e455
borditamas <borditamas@gmail.com>
2019-09-10 12:08:07 +0200

    working sr echo call

--- client-skeleton-common/src/main/java/eu/arrowhead/client/skeleton/common/ArrowheadService.java
+++ client-skeleton-common/src/main/java/eu/arrowhead/client/skeleton/common/ArrowheadService.java
@@ -1,8 +1,22 @@
 package eu.arrowhead.client.skeleton.common;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.http.HttpService;
+
+@Component
 public class ArrowheadService {
 	
-	private void echoServiceRegistry() {
-		
+	@Autowired
+	private HttpService httpService;
+	
+	public void echoServiceRegistry() {
+		ResponseEntity<String> response = httpService.sendRequest(Utilities.createURI("http", "127.0.0.1", 8443, "/serviceregistry/echo"), HttpMethod.GET, String.class);
+		System.out.println(response.getBody());
 	}
 }
--- client-skeleton-provider/src/main/java/eu/arrowhead/client/skeleton/provider/ProviderMain.java
+++ client-skeleton-provider/src/main/java/eu/arrowhead/client/skeleton/provider/ProviderMain.java
@@ -2,8 +2,13 @@ package eu.arrowhead.client.skeleton.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import eu.arrowhead.client.skeleton.common.ArrowheadService;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "eu.arrowhead")
 public class ProviderMain {
 
 	//=================================================================================================
@@ -11,6 +16,9 @@ public class ProviderMain {
 
 	//-------------------------------------------------------------------------------------------------
 	public static void main(final String[] args) {
-		SpringApplication.run(ProviderMain.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ProviderMain.class, args);
+		context.getBean(ArrowheadService.class).echoServiceRegistry();
 	}
+	
+	
 }
