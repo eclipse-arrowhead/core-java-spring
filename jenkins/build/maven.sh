6e42b41c20aba944b427e625bb441e5017fd9148
Svetlin Tanyi <svetlintanyi@gmail.com>
2019-09-12 09:38:42 +0200

    Jenkins Build

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -4,6 +4,6 @@
  echo "******* Building JAR *******"
  echo "****************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/var/lib/docker/volumes/jenkins_home/_data/workspace/Arrowhead/
 
-docker run -it -v /home/adminka/core-java-spring:/core-java-spring -v /root/.m2/:/root/.m2/ -w /core-java-spring maven:3.6.2-jdk-11-slim "$@"
+docker run -it -v $WORKSPACE/Arrowhead_Core_Spring:/core-java-spring -v /root/.m2/:/root/.m2/ -w /core-java-spring maven:3.6.2-jdk-11-slim "$@"
