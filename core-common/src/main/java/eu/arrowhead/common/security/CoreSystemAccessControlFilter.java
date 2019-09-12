f9e9fe2ea9a20f598ab45120ad99b3e1e9dc7707
rbocsi <rbocsi@aitia.ai>
2019-09-12 16:21:45 +0200

    adjustments in SR ACF needed to fix the stucked core services bug

--- core-common/src/main/java/eu/arrowhead/common/security/CoreSystemAccessControlFilter.java
+++ core-common/src/main/java/eu/arrowhead/common/security/CoreSystemAccessControlFilter.java
@@ -20,15 +20,24 @@ public abstract class CoreSystemAccessControlFilter extends AccessControlFilter 
 	
 	//-------------------------------------------------------------------------------------------------
 	protected void checkIfClientIsAnAllowedCoreSystem(final String clientCN, final String cloudCN, final CoreSystem[] allowedCoreSystems, final String requestTarget) {
+		final boolean checkResult = checkIfClientIsAnAllowedCoreSystemNoException(clientCN, cloudCN, allowedCoreSystems, requestTarget);
+
+		if (!checkResult) {
+			// client is not an allowed core system
+			log.debug("Only dedicated core systems can use {}, access denied!", requestTarget);
+			throw new AuthException(clientCN + " is unauthorized to access " + requestTarget);
+		}
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	protected boolean checkIfClientIsAnAllowedCoreSystemNoException(final String clientCN, final String cloudCN, final CoreSystem[] allowedCoreSystems, final String requestTarget) {
 		for (final CoreSystem coreSystem : allowedCoreSystems) {
 			final String coreSystemCN = coreSystem.name().toLowerCase() + "." + cloudCN;
 			if (clientCN.equalsIgnoreCase(coreSystemCN)) {
-				return;
+				return true;
 			}
 		}
 		
-		// client is not an allowed core system
-		log.debug("Only dedicated core systems can use {}, access denied!", requestTarget);
-	    throw new AuthException(clientCN + " is unauthorized to access " + requestTarget);
+		return false;
 	}
 }
--- serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/security/SRAccessControlFilter.java
+++ serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/security/SRAccessControlFilter.java
@@ -48,8 +48,8 @@ public class SRAccessControlFilter extends CoreSystemAccessControlFilter {
 			checkProviderAccessToDeregister(clientCN, queryParams, requestTarget);
 		} else if (requestTarget.endsWith(CommonConstants.OP_SERVICE_REGISTRY_QUERY_URI)) {
 			if (isClientACoreSystem(clientCN, cloudCN)) {
-				// Only dedicated core systems can use this service
-				checkIfClientIsAnAllowedCoreSystem(clientCN, cloudCN, allowedCoreSystemsForQuery, requestTarget);				
+				// Only dedicated core systems can use this service without limitation but every core system can query info about its own services
+				checkIfClientAnAllowedCoreSystemOrQueryingOwnSystems(clientCN, cloudCN, requestJSON, requestTarget); //TODO: tests
 			} else {
 				// Public core system services are allowed to query directly by the local systems
 				checkIfRequestedServiceIsAPublicCoreSystemService(requestJSON);
@@ -105,11 +105,12 @@ public class SRAccessControlFilter extends CoreSystemAccessControlFilter {
 			}
 		}
 		
-		throw new AuthException("Only public core system services are allowed to query directly. Requested service(" + requestBody.getServiceDefinitionRequirement() + ") is not!", HttpStatus.UNAUTHORIZED.value());
+		throw new AuthException("Only public core system services are allowed to query directly. Requested service (" + requestBody.getServiceDefinitionRequirement() + ") is not!",
+								HttpStatus.UNAUTHORIZED.value());
 	}
 	
 	//-------------------------------------------------------------------------------------------------
-	protected boolean  isClientACoreSystem(final String clientCN, final String cloudCN) {
+	private boolean isClientACoreSystem(final String clientCN, final String cloudCN) {
 		for (final CoreSystem coreSystem : CoreSystem.values()) {
 			final String coreSystemCN = coreSystem.name().toLowerCase() + "." + cloudCN;
 			if (clientCN.equalsIgnoreCase(coreSystemCN)) {
@@ -117,6 +118,39 @@ public class SRAccessControlFilter extends CoreSystemAccessControlFilter {
 			}
 		}		
 		return false;
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	private void checkIfClientAnAllowedCoreSystemOrQueryingOwnSystems(final String clientCN, final String cloudCN, final String requestJSON, final String requestTarget) {
+		final boolean firstCheck = checkIfClientIsAnAllowedCoreSystemNoException(clientCN, cloudCN, allowedCoreSystemsForQuery, requestTarget);
+		
+		if (!firstCheck) { // no privileged core system
+			final CoreSystem coreSystem = getClientCoreSystem(clientCN, cloudCN);
+			
+			if (coreSystem != null) {
+				final ServiceQueryFormDTO requestBody = Utilities.fromJson(requestJSON, ServiceQueryFormDTO.class);
+				
+				for (final CoreSystemService service : coreSystem.getServices()) {
+					if (service.getServiceDefinition().equalsIgnoreCase(requestBody.getServiceDefinitionRequirement().trim())) {
+						return;
+					}
+				}
+			}
+			
+			throw new AuthException("This core system only query data about its own services.", HttpStatus.UNAUTHORIZED.value());
+		}
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	private CoreSystem getClientCoreSystem(final String clientCN, final String cloudCN) {
+		for (final CoreSystem coreSystem : CoreSystem.values()) {
+			final String coreSystemCN = coreSystem.name().toLowerCase() + "." + cloudCN;
+			if (clientCN.equalsIgnoreCase(coreSystemCN)) {
+				return coreSystem;
+			}
+		}		
+		
+		return null;
 	}
 	
 	//-------------------------------------------------------------------------------------------------
