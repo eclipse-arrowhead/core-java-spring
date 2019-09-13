7d569c00e2ec0539b5ac355e6af9ee25aedc275e
borditamas <borditamas@gmail.com>
2019-09-13 09:08:09 +0200

    SrAccess filter update

--- serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/security/SRAccessControlFilter.java
+++ serviceregistry/src/main/java/eu/arrowhead/core/serviceregistry/security/SRAccessControlFilter.java
@@ -37,9 +37,7 @@ public class SRAccessControlFilter extends CoreSystemAccessControlFilter {
 		
 		final String cloudCN = getServerCloudCN();
 		
-		if (requestTarget.endsWith(CommonConstants.ECHO_URI)) {
-			// Everybody in the local cloud can test the server => no further check is necessary
-		} else if (requestTarget.contains(CommonConstants.MGMT_URI)) {
+		if (requestTarget.contains(CommonConstants.MGMT_URI)) {
 			// Only the local System Operator can use these methods
 			checkIfLocalSystemOperator(clientCN, cloudCN, requestTarget);
 		} else if (requestTarget.endsWith(CommonConstants.OP_SERVICE_REGISTRY_REGISTER_URI)) {
