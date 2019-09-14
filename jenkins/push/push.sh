58a62e5d82f03ae14649c5728e360cb1dc62acdf
Svetlin Tanyi <svetlintanyi@gmail.com>
2019-09-14 22:15:02 +0200

    Fix namespace path

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -10,20 +10,20 @@ echo "****************************"
 echo "****** Tagging images ******"
 echo "****************************"
 
-docker tag serviceregistry:$BUILD_TAG $NAMESPACE/serviceregistry:$BUILD_TAG
-docker tag authorization:$BUILD_TAG $NAMESPACE/authorization:$BUILD_TAG
-docker tag orchestrator:$BUILD_TAG $NAMESPACE/orchestrator:$BUILD_TAG
-docker tag gatekeeper:$BUILD_TAG $NAMESPACE/gatekeeper:$BUILD_TAG
-docker tag gateway:$BUILD_TAG $NAMESPACE/gateway:$BUILD_TAG
-docker tag eventhandler:$BUILD_TAG $NAMESPACE/eventhandler:$BUILD_TAG
+docker tag serviceregistry:$BUILD_TAG $NAMESPACE/$DOCKER_USER/serviceregistry:$BUILD_TAG
+docker tag authorization:$BUILD_TAG $NAMESPACE/$DOCKER_USER/authorization:$BUILD_TAG
+docker tag orchestrator:$BUILD_TAG $NAMESPACE/$DOCKER_USER/orchestrator:$BUILD_TAG
+docker tag gatekeeper:$BUILD_TAG $NAMESPACE/$DOCKER_USER/gatekeeper:$BUILD_TAG
+docker tag gateway:$BUILD_TAG $NAMESPACE/$DOCKER_USER/gateway:$BUILD_TAG
+docker tag eventhandler:$BUILD_TAG $NAMESPACE/$DOCKER_USER/eventhandler:$BUILD_TAG
 
 echo "****************************"
 echo "** Pushing images to Repo **"
 echo "****************************"
 
-docker push $NAMESPACE/serviceregistry:$BUILD_TAG
-docker push $NAMESPACE/authorization:$BUILD_TAG
-docker push $NAMESPACE/orchestrator:$BUILD_TAG
-docker push $NAMESPACE/gatekeeper:$BUILD_TAG
-docker push $NAMESPACE/gateway:$BUILD_TAG
-docker push $NAMESPACE/eventhandler:$BUILD_TAG
+docker push $NAMESPACE/$DOCKER_USER/serviceregistry:$BUILD_TAG
+docker push $NAMESPACE/$DOCKER_USER/authorization:$BUILD_TAG
+docker push $NAMESPACE/$DOCKER_USER/orchestrator:$BUILD_TAG
+docker push $NAMESPACE/$DOCKER_USER/gatekeeper:$BUILD_TAG
+docker push $NAMESPACE/$DOCKER_USER/gateway:$BUILD_TAG
+docker push $NAMESPACE/$DOCKER_USER/eventhandler:$BUILD_TAG
