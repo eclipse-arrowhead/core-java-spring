693667881a9079e095448e92a1afbb468de1deaf
Svetlin Tanyi <svetlintanyi@gmail.com>
2019-09-16 11:20:57 +0200

    build latest

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -17,6 +17,14 @@ docker tag gatekeeper:$BUILD_TAG $NAMESPACE/$DOCKER_USER/gatekeeper:$BUILD_TAG
 docker tag gateway:$BUILD_TAG $NAMESPACE/$DOCKER_USER/gateway:$BUILD_TAG
 docker tag eventhandler:$BUILD_TAG $NAMESPACE/$DOCKER_USER/eventhandler:$BUILD_TAG
 
+
+docker tag serviceregistry:$BUILD_TAG $NAMESPACE/$DOCKER_USER/serviceregistry:latest
+docker tag authorization:$BUILD_TAG $NAMESPACE/$DOCKER_USER/authorization:latest
+docker tag orchestrator:$BUILD_TAG $NAMESPACE/$DOCKER_USER/orchestrator:latest
+docker tag gatekeeper:$BUILD_TAG $NAMESPACE/$DOCKER_USER/gatekeeper:latest
+docker tag gateway:$BUILD_TAG $NAMESPACE/$DOCKER_USER/gateway:latest
+docker tag eventhandler:$BUILD_TAG $NAMESPACE/$DOCKER_USER/eventhandler:latest
+
 echo "****************************"
 echo "** Pushing images to Repo **"
 echo "****************************"
@@ -27,3 +35,10 @@ docker push $NAMESPACE/$DOCKER_USER/orchestrator:$BUILD_TAG
 docker push $NAMESPACE/$DOCKER_USER/gatekeeper:$BUILD_TAG
 docker push $NAMESPACE/$DOCKER_USER/gateway:$BUILD_TAG
 docker push $NAMESPACE/$DOCKER_USER/eventhandler:$BUILD_TAG
+
+docker push $NAMESPACE/$DOCKER_USER/serviceregistry:latest
+docker push $NAMESPACE/$DOCKER_USER/authorization:latest
+docker push $NAMESPACE/$DOCKER_USER/orchestrator:latest
+docker push $NAMESPACE/$DOCKER_USER/gatekeeper:latest
+docker push $NAMESPACE/$DOCKER_USER/gateway:latest
+docker push $NAMESPACE/$DOCKER_USER/eventhandler:latest
