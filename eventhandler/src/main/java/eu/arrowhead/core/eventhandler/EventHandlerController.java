eb1f4837b81bd73da637296ad9dc8f07ee84f04a
Gabor Majoros <gabormajo@yahoo.com>
2019-09-17 12:17:55 +0200

    requested change: rename method to verb

--- eventhandler/src/main/java/eu/arrowhead/core/eventhandler/EventHandlerController.java
+++ eventhandler/src/main/java/eu/arrowhead/core/eventhandler/EventHandlerController.java
@@ -230,7 +230,7 @@ public class EventHandlerController {
 			@ApiResponse(code = HttpStatus.SC_INTERNAL_SERVER_ERROR, message = CommonConstants.SWAGGER_HTTP_500_MESSAGE)
 	})
 	@PostMapping(path = CommonConstants.OP_EVENT_HANDLER_SUBSCRIBE, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody public SubscriptionResponseDTO subscription(@RequestBody final SubscriptionRequestDTO request) {
+	@ResponseBody public SubscriptionResponseDTO subscribe(@RequestBody final SubscriptionRequestDTO request) {
 		logger.debug("subscription started ...");
 		
 		final String origin = CommonConstants.EVENT_HANDLER_URI + CommonConstants.OP_EVENT_HANDLER_SUBSCRIBE;
@@ -248,7 +248,7 @@ public class EventHandlerController {
 			@ApiResponse(code = HttpStatus.SC_INTERNAL_SERVER_ERROR, message = CommonConstants.SWAGGER_HTTP_500_MESSAGE)
 	})
 	@PutMapping(path = CommonConstants.OP_EVENT_HANDLER_SUBSCRIBE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody public void unSubscription(@RequestBody final SubscriptionRequestDTO request) {
+	@ResponseBody public void unSubscribe(@RequestBody final SubscriptionRequestDTO request) {
 		logger.debug("unSubscription started ...");
 		
 		final String origin = CommonConstants.EVENT_HANDLER_URI + CommonConstants.OP_EVENT_HANDLER_SUBSCRIBE;
