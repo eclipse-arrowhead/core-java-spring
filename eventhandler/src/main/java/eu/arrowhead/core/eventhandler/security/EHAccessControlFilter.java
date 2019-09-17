62cd764da2f7ebfa887b111da9df815ea5162202
Gabor Majoros <gabormajo@yahoo.com>
2019-09-17 19:19:40 +0200

    requested change:  make sure that echo can be called by anybody in the cloud

--- eventhandler/src/main/java/eu/arrowhead/core/eventhandler/security/EHAccessControlFilter.java
+++ eventhandler/src/main/java/eu/arrowhead/core/eventhandler/security/EHAccessControlFilter.java
@@ -33,7 +33,11 @@ public class EHAccessControlFilter extends CoreSystemAccessControlFilter {
 		super.checkClientAuthorized(clientCN, method, requestTarget, requestJSON, queryParams);
 		
 		final String cloudCN = getServerCloudCN();
-		if ( requestTarget.contains( CommonConstants.MGMT_URI ) ) {
+		
+		if (requestTarget.endsWith(CommonConstants.ECHO_URI)) {
+			// Everybody in the local cloud can test the server => no further check is necessary
+		
+		} else if ( requestTarget.contains( CommonConstants.MGMT_URI ) ) {
 			// Only the local System Operator can use these methods
 			checkIfLocalSystemOperator(clientCN, cloudCN, requestTarget);
 		
