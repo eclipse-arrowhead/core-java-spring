ab19869ca7ba0340b0ee85be65b80396f1c65388
Gabor Majoros <gabormajo@yahoo.com>
2019-10-04 13:56:18 +0200

    requested change: should not use shutdown here

--- eventhandler/src/main/java/eu/arrowhead/core/eventhandler/publish/PublishRequestFixedExecutor.java
+++ eventhandler/src/main/java/eu/arrowhead/core/eventhandler/publish/PublishRequestFixedExecutor.java
@@ -71,7 +71,6 @@ public class PublishRequestFixedExecutor {
 			}
 		}
 		
-		threadPool.shutdown();
 	}
 
 	//-------------------------------------------------------------------------------------------------
