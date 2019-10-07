e10bcfdd6a17c00b1cf692f0a6241808215f2ed7
Gabor Majoros <gabormajo@yahoo.com>
2019-10-07 16:40:14 +0200

    bugfix: use shutdown at validationExceptions

--- eventhandler/src/main/java/eu/arrowhead/core/eventhandler/publish/PublishRequestExecutor.java
+++ eventhandler/src/main/java/eu/arrowhead/core/eventhandler/publish/PublishRequestExecutor.java
@@ -70,9 +70,19 @@ public class PublishRequestExecutor {
 	
 	//-------------------------------------------------------------------------------------------------
 	private void validateMembers() {
-		Assert.notNull(this.threadPool, "threadPool is null");
-		Assert.notNull(this.publishRequestDTO, "publishRequestDTO is null");
-		Assert.notNull(this.involvedSubscriptions, "involvedSubscriptions is null");
-		Assert.notNull(this.httpService, "httpService is null");
+
+		try {
+			
+			Assert.notNull(this.threadPool, "threadPool is null");
+			Assert.notNull(this.publishRequestDTO, "publishRequestDTO is null");
+			Assert.notNull(this.involvedSubscriptions, "involvedSubscriptions is null");
+			Assert.notNull(this.httpService, "httpService is null");
+			
+		} catch (final IllegalArgumentException  ex) {
+			
+			threadPool.isShutdown();
+			
+			throw ex;
+		}
 	}
 }
