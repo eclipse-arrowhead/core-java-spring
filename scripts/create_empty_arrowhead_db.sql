639434de76e4eb3133bb82c99fad26930b44cc53
Gabor Majoros <gabormajo@yahoo.com>
2019-10-07 12:41:39 +0200

    bugfix: change eventype_name form default null to not null

--- scripts/create_empty_arrowhead_db.sql
+++ scripts/create_empty_arrowhead_db.sql
@@ -248,7 +248,7 @@ CREATE TABLE `logs` (
 DROP TABLE IF EXISTS `event_type`;
   CREATE TABLE `event_type` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `event_type_name` varchar(255) DEFAULT NULL,
+  `event_type_name` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
