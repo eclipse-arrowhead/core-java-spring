ea629df3c24553ce128dc64879a54872cf4817c5
Svetlin Tanyi <svetlintanyi@gmail.com>
2019-10-28 11:03:44 +0100

    Datamanager Skeleton

---
+++ datamanager/src/main/java/eu/arrowhead/core/datamanager/DatamanagerController.java
@@ -0,0 +1,61 @@
+package eu.arrowhead.core.datamanager;
+
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.CoreDefaults;
+import eu.arrowhead.common.CoreUtilities;
+import eu.arrowhead.common.CoreCommonConstants;
+import eu.arrowhead.common.Defaults;
+import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.exception.BadPayloadException;
+import eu.arrowhead.core.datamanager.database.service.DatamanagerDBService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.apache.http.HttpStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Api(tags = { CoreCommonConstants.SWAGGER_TAG_ALL })
+@CrossOrigin(maxAge = Defaults.CORS_MAX_AGE, allowCredentials = Defaults.CORS_ALLOW_CREDENTIALS,
+    allowedHeaders = { HttpHeaders.ORIGIN, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT, HttpHeaders.AUTHORIZATION }
+)
+@RestController
+@RequestMapping(CommonConstants.DATAMANAGER_URI)
+public class DatamanagerController {
+
+    private final Logger logger = LogManager.getLogger(DatamanagerController.class);
+
+    @Autowired
+    private DatamanagerDBService datamanagerDBService;
+
+    @ApiOperation(value = "Return an echo message with the purpose of testing the core service availability",
+            response = String.class,
+            tags = { CoreCommonConstants.SWAGGER_TAG_CLIENT })
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpStatus.SC_OK, message = CoreCommonConstants.SWAGGER_HTTP_200_MESSAGE),
+            @ApiResponse(code = HttpStatus.SC_UNAUTHORIZED, message = CoreCommonConstants.SWAGGER_HTTP_401_MESSAGE),
+            @ApiResponse(code = HttpStatus.SC_INTERNAL_SERVER_ERROR, message = CoreCommonConstants.SWAGGER_HTTP_500_MESSAGE)
+    })
+    @GetMapping(path = CommonConstants.ECHO_URI)
+    public String echoService() {
+        return "Got it!";
+    }
+}
---
+++ datamanager/src/main/java/eu/arrowhead/core/datamanager/security/DatamanagerAccessControlFilter.java
@@ -0,0 +1,24 @@
+package eu.arrowhead.core.datamanager.security;
+
+import java.util.Map;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.security.AccessControlFilter;
+
+@Component
+@ConditionalOnProperty(name = CommonConstants.SERVER_SSL_ENABLED, matchIfMissing = true) 
+public class DatamanagerAccessControlFilter extends AccessControlFilter {
+	
+	//=================================================================================================
+	// assistant methods
+
+	//-------------------------------------------------------------------------------------------------
+	@Override
+	protected void checkClientAuthorized(final String clientCN, final String method, final String requestTarget, final String requestJSON, final Map<String,String[]> queryParams) {
+		super.checkClientAuthorized(clientCN, method, requestTarget, requestJSON, queryParams);
+		//TODO: implement datamanager specific logic here
+	}
+}
