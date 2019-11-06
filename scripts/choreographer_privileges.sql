f91d94ebea3a029a9a50d80af48357471d1f2bb1
Kristóf Szabó <szaboo.kris@gmail.com>
2019-11-06 11:50:02 +0100

    Added choreographer privilages to the newly added tables.

--- scripts/choreographer_privileges.sql
+++ scripts/choreographer_privileges.sql
@@ -10,6 +10,9 @@ GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_action_action_step_connection
 GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_action_step_service_definition_connection` TO 'choreographer'@'localhost';
 GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_next_action_step` TO 'choreographer'@'localhost';
 GRANT ALL PRIVILEGES ON `arrowhead`.`service_definition` TO 'choreographer'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_workspace` TO 'choreographer'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_workspace_system_connection` TO 'choreographer'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`system_` TO 'choreographer'@'localhost';
 GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'choreographer'@'localhost';
 
 REVOKE ALL, GRANT OPTION FROM 'choreographer'@'%';
@@ -22,6 +25,8 @@ GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_action_action_step_connection
 GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_action_step_service_definition_connection` TO 'choreographer'@'%';
 GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_next_action_step` TO 'choreographer'@'%';
 GRANT ALL PRIVILEGES ON `arrowhead`.`service_definition` TO 'choreographer'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`choreographer_workspace_system_connection` TO 'choreographer'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`system_` TO 'choreographer'@'%';
 GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'choreographer'@'%';
 
 FLUSH PRIVILEGES;
