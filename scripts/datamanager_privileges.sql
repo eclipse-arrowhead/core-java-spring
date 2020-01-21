b751f8185da5eaaefa6cbd8496c7c59239152da1
Jens Eliasson <jens.eliasson@gmail.com>
2020-01-21 17:47:31 +0100

    added file

---
+++ scripts/datamanager_privileges.sql
@@ -0,0 +1,15 @@
+USE `arrowhead`;
+
+GRANT ALL PRIVILEGES ON `arrowhead`.`dmhist_services` TO 'datamanager'@'localhost';
+#GRANT ALL PRIVILEGES ON `arrowhead`.`dmhist_files` TO 'datamanager'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`dmhist_messages` TO 'datamanager'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`dmhist_entries` TO 'datamanager'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'datamanager'@'localhost';
+
+#GRANT ALL PRIVILEGES ON `arrowhead`.`dmhist_services` TO 'datamanager'@'%';
+#GRANT ALL PRIVILEGES ON `arrowhead`.`dmhist_files` TO 'datamanager'@'%';
+#GRANT ALL PRIVILEGES ON `arrowhead`.`dmhist_messages` TO 'datamanager'@'%';
+#GRANT ALL PRIVILEGES ON `arrowhead`.`dmhist_entries` TO 'datamanager'@'%';
+#GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'datamanager'@'%';
+
+FLUSH PRIVILEGES;
