6176751bb0088fabce8432298042a76ab40d8145
rbocsi <rbocsi@aitia.ai>
2020-01-22 16:07:52 +0100

    empty db service class renaming repositories

--- core-common/src/main/java/eu/arrowhead/common/database/repository/QosIntraMeasurementRepository.java
+++ core-common/src/main/java/eu/arrowhead/common/database/repository/QosIntraMeasurementRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import eu.arrowhead.common.database.entity.QoSIntraMeasurement;
 
 @Repository
-public interface QosIntraMeasurementRepository extends RefreshableRepository<QoSIntraMeasurement,Long> {
+public interface QoSIntraMeasurementRepository extends RefreshableRepository<QoSIntraMeasurement,Long> {
 
 	//=================================================================================================
 	// methods
