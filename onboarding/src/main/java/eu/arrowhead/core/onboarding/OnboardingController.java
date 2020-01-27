dc2d0dd04cd80feac9c6191cee07aed5acefcdaf
mzsilak <mario.zsilak@gmail.com>
2020-01-27 13:24:33 +0100

    fixed wildcard imports in controllers

--- onboarding/src/main/java/eu/arrowhead/core/onboarding/OnboardingController.java
+++ onboarding/src/main/java/eu/arrowhead/core/onboarding/OnboardingController.java
@@ -1,9 +1,14 @@
 package eu.arrowhead.core.onboarding;
 
-import eu.arrowhead.common.*;
-import eu.arrowhead.common.dto.internal.DeviceListResponseDTO;
-import eu.arrowhead.common.dto.internal.DeviceRegistryListResponseDTO;
-import eu.arrowhead.common.dto.shared.*;
+import eu.arrowhead.common.CommonConstants;
+import eu.arrowhead.common.CoreCommonConstants;
+import eu.arrowhead.common.Defaults;
+import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.dto.shared.DeviceResponseDTO;
+import eu.arrowhead.common.dto.shared.OnboardingWithCsrRequestDTO;
+import eu.arrowhead.common.dto.shared.OnboardingWithCsrResponseDTO;
+import eu.arrowhead.common.dto.shared.OnboardingWithNameRequestDTO;
+import eu.arrowhead.common.dto.shared.OnboardingWithNameResponseDTO;
 import eu.arrowhead.common.exception.BadPayloadException;
 import eu.arrowhead.core.onboarding.database.service.OnboardingDBService;
 import io.swagger.annotations.Api;
@@ -15,14 +20,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.cert.X509Certificate;
-import java.time.format.DateTimeParseException;
 import java.util.Objects;
-import java.util.regex.Matcher;
 
 @Api(tags = {CoreCommonConstants.SWAGGER_TAG_ALL})
 @CrossOrigin(maxAge = Defaults.CORS_MAX_AGE, allowCredentials = Defaults.CORS_ALLOW_CREDENTIALS,
