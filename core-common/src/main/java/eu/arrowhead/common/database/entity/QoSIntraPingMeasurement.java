05e7abf750e137f9117dd9a53ebc94efc8dc2a50
Gabor Majoros <gabormajo@yahoo.com>
2020-01-28 16:11:03 +0100

    updated column names

--- core-common/src/main/java/eu/arrowhead/common/database/entity/QoSIntraPingMeasurement.java
+++ core-common/src/main/java/eu/arrowhead/common/database/entity/QoSIntraPingMeasurement.java
@@ -41,9 +41,21 @@ public class QoSIntraPingMeasurement {
 	@Column(name = "max_response_time", nullable = true)
 	private Integer maxResponseTime;
 
-	@Column(name = "mean_response_time", nullable = true)
-	private Integer meanResponseTime;
-	
+	@Column(name = "mean_response_time_with_timeout", nullable = true)
+	private Integer meanResponseTimeWithTimeout;
+
+	@Column(name = "mean_response_time_without_timeout", nullable = true)
+	private Integer meanResponseTimeWithoutTimeout;
+
+	@Column(name = "jitter_with_timeout", nullable = true)
+	private Integer jitterWithTimeout;
+
+	@Column(name = "jitter_without_timeout", nullable = true)
+	private Integer jitterWithoutTimeout;
+
+	@Column(name = "lost_per_measurement_percent", nullable = false)
+	private Integer lostPerMeasurementPercent;
+
 	@Column(name = "sent", nullable = false)
 	private long sent;
 
@@ -91,7 +103,11 @@ public class QoSIntraPingMeasurement {
 	public ZonedDateTime getLastAccessAt() { return lastAccessAt; }
 	public Integer getMinResponseTime() { return minResponseTime; }
 	public Integer getMaxResponseTime() { return maxResponseTime; }
-	public Integer getMeanResponseTime() { return meanResponseTime; }
+	public Integer getMeanResponseTimeWithTimeout() { return meanResponseTimeWithTimeout; }
+	public Integer getMeanResponseTimeWithoutTimeout() { return meanResponseTimeWithoutTimeout; }
+	public Integer getJitterWithTimeout() {return jitterWithTimeout;}
+	public Integer getJitterWithoutTimeout() { return jitterWithoutTimeout; }
+	public Integer getLostPerMeasurementPercent() { return lostPerMeasurementPercent; }
 	public long getSent() { return sent; }
 	public long getReceived() { return received; }
 	public ZonedDateTime getCountStartedAt() { return countStartedAt; }
@@ -107,7 +123,11 @@ public class QoSIntraPingMeasurement {
 	public void setLastAccessAt(final ZonedDateTime lastAccessAt) { this.lastAccessAt = lastAccessAt; }
 	public void setMinResponseTime(final Integer minResponseTime) { this.minResponseTime = minResponseTime; }
 	public void setMaxResponseTime(final Integer maxResponseTime) { this.maxResponseTime = maxResponseTime; }
-	public void setMeanResponseTime(final Integer meanResponseTime) { this.meanResponseTime = meanResponseTime; }
+	public void setMeanResponseTimeWithoutTimeout(final Integer meanResponseTimeWithoutTimeout) { this.meanResponseTimeWithoutTimeout = meanResponseTimeWithoutTimeout; }
+	public void setMeanResponseTimeWithTimeout(final Integer meanResponseTimeWithTimeout) { this.meanResponseTimeWithTimeout = meanResponseTimeWithTimeout; }
+	public void setJitterWithTimeout(Integer jitterWithTimeout) { this.jitterWithTimeout = jitterWithTimeout; }
+	public void setJitterWithoutTimeout(Integer jitterWithoutTimeout) { this.jitterWithoutTimeout = jitterWithoutTimeout; }
+	public void setLostPerMeasurementPercent(Integer lostPerMeasurementPercent) { this.lostPerMeasurementPercent = lostPerMeasurementPercent; }
 	public void setSent(final long sent) { this.sent = sent; }
 	public void setReceived(final long received) { this.received = received; }
 	public void setCountStartedAt(final ZonedDateTime countStartedAt) { this.countStartedAt = countStartedAt; }
