04e4d73b658c3ffde1a8e5658604d04fb76542f6
Gabor Majoros <gabormajo@yahoo.com>
2020-01-31 15:00:04 +0100

    requested change: move method to DBService

--- qos-monitor/src/main/java/eu/arrowhead/core/qos/database/service/QoSDatabaseService.java
+++ qos-monitor/src/main/java/eu/arrowhead/core/qos/database/service/QoSDatabaseService.java
@@ -1,20 +1,61 @@
 package eu.arrowhead.core.qos.database.service;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import eu.arrowhead.common.database.entity.QoSIntraPingMeasurement;
 import eu.arrowhead.common.database.repository.QoSIntraMeasurementPingRepository;
 import eu.arrowhead.common.database.repository.QoSIntraMeasurementRepository;
+import eu.arrowhead.common.database.repository.QoSIntraPingMeasurementLogDetailsRepository;
+import eu.arrowhead.common.database.repository.QoSIntraPingMeasurementLogRepository;
+import eu.arrowhead.common.database.repository.SystemRepository;
+import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.core.qos.quartz.task.CountRestarterTask;
 
 @Service
 public class QoSDatabaseService {
 
 	//=================================================================================================
 	// members
-	
+
 	@Autowired
 	private QoSIntraMeasurementRepository qosIntraMeasurementRepository;
-	
+
 	@Autowired
-	private QoSIntraMeasurementPingRepository qosIntraMeasurementPingRepository;
+	private QoSIntraMeasurementPingRepository qoSIntraMeasurementPingRepository;
+
+	@Autowired
+	private QoSIntraPingMeasurementLogRepository qoSIntraPingMeasurementLogRepository;
+
+	@Autowired
+	private QoSIntraPingMeasurementLogDetailsRepository qoSIntraPingMeasurementLogDetailsRepository;
+
+	@Autowired
+	private SystemRepository systemRepository;
+
+	protected Logger logger = LogManager.getLogger(QoSDatabaseService.class);
+	//=================================================================================================
+	// methods
+
+	//-------------------------------------------------------------------------------------------------	
+	@Transactional (rollbackFor = ArrowheadException.class)
+	public void updateCountStartedAt() {
+		logger.debug("updateCountStartedAt started...");
+
+		final List<QoSIntraPingMeasurement> measurementList = qoSIntraMeasurementPingRepository.findAll();
+		for (final QoSIntraPingMeasurement qoSIntraPingMeasurement : measurementList) {
+			qoSIntraPingMeasurement.setSent(0);
+			qoSIntraPingMeasurement.setReceived(0);
+			qoSIntraPingMeasurement.setCountStartedAt(ZonedDateTime.now());
+		}
+		qoSIntraMeasurementPingRepository.saveAll(measurementList);
+		qoSIntraMeasurementPingRepository.flush();
+
+	}
 }
--- qos-monitor/src/main/java/eu/arrowhead/core/qos/quartz/task/CountRestarterTask.java
+++ qos-monitor/src/main/java/eu/arrowhead/core/qos/quartz/task/CountRestarterTask.java
@@ -1,8 +1,5 @@
 package eu.arrowhead.core.qos.quartz.task;
 
-import java.time.ZonedDateTime;
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.quartz.DisallowConcurrentExecution;
@@ -11,11 +8,8 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
-import eu.arrowhead.common.database.entity.QoSIntraPingMeasurement;
-import eu.arrowhead.common.database.repository.QoSIntraMeasurementPingRepository;
-import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.core.qos.database.service.QoSDatabaseService;
 
 @Component
 @DisallowConcurrentExecution
@@ -27,7 +21,7 @@ public class CountRestarterTask implements Job {
 	protected Logger logger = LogManager.getLogger(CountRestarterTask.class);
 
 	@Autowired
-	private QoSIntraMeasurementPingRepository qoSIntraMeasurementPingRepository;
+	private QoSDatabaseService qoSDatabaseService;
 
 	//=================================================================================================
 	// methods
@@ -37,25 +31,11 @@ public class CountRestarterTask implements Job {
 	public void execute(final JobExecutionContext context) throws JobExecutionException {
 		logger.debug("STARTED: count restarter task");
 
-		updateCountStartedAt();
+		qoSDatabaseService.updateCountStartedAt();
 
 		logger.debug("Finished: count restarter task");
 	}
 
-	@Transactional (rollbackFor = ArrowheadException.class)
-	public void updateCountStartedAt() {
-		logger.debug("updateCountStartedAt started...");
-
-		final List<QoSIntraPingMeasurement> measurementList = qoSIntraMeasurementPingRepository.findAll();
-		for (final QoSIntraPingMeasurement qoSIntraPingMeasurement : measurementList) {
-			qoSIntraPingMeasurement.setSent(0);
-			qoSIntraPingMeasurement.setReceived(0);
-			qoSIntraPingMeasurement.setCountStartedAt(ZonedDateTime.now());
-
-			qoSIntraMeasurementPingRepository.saveAndFlush(qoSIntraPingMeasurement);
-
-		}
-	}
 	//=================================================================================================
 	// assistant methods
 	
