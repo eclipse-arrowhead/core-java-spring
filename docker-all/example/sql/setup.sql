c9148f4870659cd77418b1245b47404e21664086
Kovács Kristóf <kristof.kovacs@eilabs.com>
2020-02-25 21:12:43 +0100

    Add CA to Docker related files

--- docker-all/example/sql/setup.sql
+++ docker-all/example/sql/setup.sql
@@ -537,4 +537,45 @@ GRANT ALL PRIVILEGES ON `arrowhead`.`service_definition` TO 'gateway'@'%';
 GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_inter_cloud` TO 'gateway'@'%';
 GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'gateway'@'%';
 
+-- Certificate Authority
+DROP USER IF EXISTS 'certificate_authority'@'localhost';
+CREATE USER IF NOT EXISTS 'certificate_authority'@'localhost' IDENTIFIED BY 'FsdG6Kgf9QpPfv2';
+GRANT ALL PRIVILEGES ON `arrowhead`.`system_` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_definition` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_interface` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`certificate_authority` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`certificate_authority_interface_connection` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`cloud` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`relay` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`cloud_gatekeeper_relay` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`cloud_gateway_relay` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_intra_cloud` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_intra_cloud_interface_connection` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_inter_cloud` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_inter_cloud_interface_connection` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`orchestrator_store` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`subscription` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`subscription_publisher_connection` TO 'certificate_authority'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'certificate_authority'@'localhost';
+
+DROP USER IF EXISTS 'certificate_authority'@'%';
+CREATE USER IF NOT EXISTS 'certificate_authority'@'%' IDENTIFIED BY 'FsdG6Kgf9QpPfv2';
+GRANT ALL PRIVILEGES ON `arrowhead`.`system_` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_definition` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_interface` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`certificate_authority` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`certificate_authority_interface_connection` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`cloud` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`relay` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`cloud_gatekeeper_relay` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`cloud_gateway_relay` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_intra_cloud` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_intra_cloud_interface_connection` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_inter_cloud` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`authorization_inter_cloud_interface_connection` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`orchestrator_store` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`subscription` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`subscription_publisher_connection` TO 'certificate_authority'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'certificate_authority'@'%';
+
 FLUSH PRIVILEGES;
