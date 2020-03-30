943524d242148a971fc30287768085340b7eea7a
Tamas Bordi <borditamas@gmail.com>
2020-03-30 13:49:34 +0200

    update QosMonitorAccessControlFilterTest

--- orchestrator/src/main/java/eu/arrowhead/core/orchestrator/security/OrchestratorAccessControlFilter.java
+++ orchestrator/src/main/java/eu/arrowhead/core/orchestrator/security/OrchestratorAccessControlFilter.java
@@ -32,7 +32,7 @@ public class OrchestratorAccessControlFilter extends CoreSystemAccessControlFilt
 			// Only the local System Operator can use these methods
 			checkIfLocalSystemOperator(clientCN, cloudCN, requestTarget);
 		} else if (requestTarget.contains(CommonConstants.OP_ORCH_QOS_ENABLED_URI) || requestTarget.contains(CommonConstants.OP_ORCH_QOS_RESERVATIONS_URI) ||
-				   requestTarget.contains(CommonConstants.OP_ORCH_QOS_TEMPORARY_LOCK_URI) || requestTarget.contains(CommonConstants.OP_ORCH_QOS_RESERVATIONS_URI)) {
+				   requestTarget.contains(CommonConstants.OP_ORCH_QOS_TEMPORARY_LOCK_URI)) {
 			// Only the local Gatekeeper can use these methods
 			final CoreSystem[] allowedCoreSystems = { CoreSystem.GATEKEEPER };
 			checkIfClientIsAnAllowedCoreSystem(clientCN, cloudCN, allowedCoreSystems, requestTarget);
