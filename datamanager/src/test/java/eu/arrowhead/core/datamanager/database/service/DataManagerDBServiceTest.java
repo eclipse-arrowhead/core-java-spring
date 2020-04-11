35342485ce04e3f2941e328a763be822e32af325
Jens Eliasson <jens.eliasson@gmail.com>
2020-04-11 14:20:27 +0200

    added initial support for DataManager unit testing

--- datamanager/src/test/java/eu/arrowhead/core/datamanager/database/service/DataManagerDBServiceTest.java
+++ datamanager/src/test/java/eu/arrowhead/core/datamanager/database/service/DataManagerDBServiceTest.java
@@ -1,32 +1,64 @@
 package eu.arrowhead.core.datamanager.database.service;
 
-import eu.arrowhead.common.Utilities;
-import eu.arrowhead.common.database.entity.ServiceDefinition;
-import eu.arrowhead.common.database.repository.*;
-import eu.arrowhead.common.exception.InvalidParameterException;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import static org.mockito.ArgumentMatchers.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import java.time.ZonedDateTime;
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
-
-import static reactor.core.publisher.Mono.when;
+import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.dto.internal.DTOConverter;
+import eu.arrowhead.common.exception.InvalidParameterException;
+import eu.arrowhead.core.datamanager.service.DataManagerDriver;
+import eu.arrowhead.core.datamanager.service.DataManagerService;
 
 @RunWith(SpringRunner.class)
 public class DataManagerDBServiceTest {
 
-    @InjectMocks
-    private DataManagerDBService datamanagerDBService;
+    //=================================================================================================
+    // members
+
+    //@InjectMocks
+    //private DataManagerDBService datamanagerDBService = new DataManagerDBService();
+
+    @Mock
+    private DataManagerService dataManagerService;
+	
+    @Mock
+    private DataManagerDriver dataManagerDriver;
+
+    //=================================================================================================
+    // methods
+    
+    @Test
+    public void getServicesFromFakeSystem() {
+	/*final ArrayList<String> response = datamanagerDBService.getServicesFromSystem("nonExistingFakeTestOnlySystem");
+	
+	assertNotNull(response);
+	assertTrue(response.size() == 0);*/
+    }
 }
+
