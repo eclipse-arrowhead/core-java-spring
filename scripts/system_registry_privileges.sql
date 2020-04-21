14eec580d2f7f13d42ad82a0b8cbef014e527230
Mario Zsilak <mario.zsilak@gmail.com>
2020-04-21 09:41:28 +0200

    added database privileges for system and device registry

--- scripts/system_registry_privileges.sql
+++ scripts/system_registry_privileges.sql
@@ -2,16 +2,24 @@ USE `arrowhead`;
 
 REVOKE ALL, GRANT OPTION FROM 'system_registry'@'localhost';
 
+GRANT ALL PRIVILEGES ON `arrowhead`.`device` TO 'system_registry'@'localhost';
 GRANT ALL PRIVILEGES ON `arrowhead`.`system_` TO 'system_registry'@'localhost';
 GRANT ALL PRIVILEGES ON `arrowhead`.`system_registry` TO 'system_registry'@'localhost';
-GRANT ALL PRIVILEGES ON `arrowhead`.`device` TO 'system_registry'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_definition` TO 'system_registry'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_interface` TO 'system_registry'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_registry` TO 'system_registry'@'localhost';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_registry_interface_connection` TO 'system_registry'@'localhost';
 GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'system_registry'@'localhost';
 
 REVOKE ALL, GRANT OPTION FROM 'system_registry'@'%';
 
+GRANT ALL PRIVILEGES ON `arrowhead`.`device` TO 'system_registry'@'%';
 GRANT ALL PRIVILEGES ON `arrowhead`.`system_` TO 'system_registry'@'%';
 GRANT ALL PRIVILEGES ON `arrowhead`.`system_registry` TO 'system_registry'@'%';
-GRANT ALL PRIVILEGES ON `arrowhead`.`device` TO 'system_registry'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_definition` TO 'system_registry'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_interface` TO 'system_registry'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_registry` TO 'system_registry'@'%';
+GRANT ALL PRIVILEGES ON `arrowhead`.`service_registry_interface_connection` TO 'system_registry'@'%';
 GRANT ALL PRIVILEGES ON `arrowhead`.`logs` TO 'system_registry'@'%';
 
 FLUSH PRIVILEGES;
