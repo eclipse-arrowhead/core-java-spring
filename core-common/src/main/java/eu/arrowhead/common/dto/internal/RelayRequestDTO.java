fa94d49ccad61ef1d8adb47bdbb236c8504146b7
Tamas Bordi <borditamas@gmail.com>
2020-04-27 17:06:06 +0200

    add hash and equals to RelayRequestDTO

--- core-common/src/main/java/eu/arrowhead/common/dto/internal/RelayRequestDTO.java
+++ core-common/src/main/java/eu/arrowhead/common/dto/internal/RelayRequestDTO.java
@@ -42,5 +42,61 @@ public class RelayRequestDTO implements Serializable {
 	public void setPort(final Integer port) { this.port = port; }
 	public void setSecure(final boolean secure) { this.secure = secure; }
 	public void setExclusive(final boolean exclusive) { this.exclusive = exclusive; }
-	public void setType(final String type) { this.type = type; }	
+	public void setType(final String type) { this.type = type; }
+
+	//-------------------------------------------------------------------------------------------------
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + (exclusive ? 1231 : 1237);
+		result = prime * result + ((port == null) ? 0 : port.hashCode());
+		result = prime * result + (secure ? 1231 : 1237);
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		RelayRequestDTO other = (RelayRequestDTO) obj;
+		if (address == null) {
+			if (other.address != null) {
+				return false;
+			}
+		} else if (!address.equals(other.address)) {
+			return false;
+		}
+		if (exclusive != other.exclusive) {
+			return false;
+		}
+		if (port == null) {
+			if (other.port != null) {
+				return false;
+			}
+		} else if (!port.equals(other.port)) {
+			return false;
+		}
+		if (secure != other.secure) {
+			return false;
+		}
+		if (type == null) {
+			if (other.type != null) {
+				return false;
+			}
+		} else if (!type.equals(other.type)) {
+			return false;
+		}
+		return true;
+	}		
 }
