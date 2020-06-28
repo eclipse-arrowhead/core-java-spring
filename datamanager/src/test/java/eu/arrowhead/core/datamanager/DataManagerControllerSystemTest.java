acafbc97bf0874054b90827ce96e7e0029fb878e
Jens Eliasson <jens.eliasson@gmail.com>
2020-06-28 15:46:46 +0200

    added support for testing

---
+++ datamanager/src/test/java/eu/arrowhead/core/datamanager/DataManagerControllerSystemTest.java
@@ -0,0 +1,84 @@
+package eu.arrowhead.core.datamanager;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.arrowhead.common.dto.internal.SystemListResponseDTO;
+import eu.arrowhead.common.dto.shared.SystemRequestDTO;
+import eu.arrowhead.common.dto.shared.SystemResponseDTO;
+import eu.arrowhead.common.exception.InvalidParameterException;
+import eu.arrowhead.core.datamanager.database.service.DataManagerDBService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = {DataManagerTestContext.class})
+public class DataManagerControllerSystemTest {
+
+    //=================================================================================================
+    // test data
+
+    //=================================================================================================
+    // members
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean(name = "mockDataManagerDBService")
+    private DataManagerDBService dataManagerDBService;
+
+    //=================================================================================================
+    // methods
+
+    //-------------------------------------------------------------------------------------------------
+    @Before
+    public void setup() {
+        reset(dataManagerDBService);
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    //=================================================================================================
+    // Tests of Historian echo service
+
+    @Test
+    public void echoHistorian() throws Exception {
+        final MvcResult response = this.mockMvc.perform(get("/datamanager/echo")
+                                                                .accept(MediaType.APPLICATION_JSON))
+                                               .andExpect(status().isOk())
+                                               .andReturn();
+        assertEquals("Got it!", response.getResponse().getContentAsString());
+    }
+}
---
+++ datamanager/src/test/java/eu/arrowhead/core/datamanager/StandaloneModeInDataManagerTests.java
@@ -0,0 +1,9 @@
+package eu.arrowhead.core.datamanager;
+
+import eu.arrowhead.common.testhelper.StandaloneModeInTests;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StandaloneModeInDataManagerTests extends StandaloneModeInTests {
+
+}
