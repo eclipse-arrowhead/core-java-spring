07b76ba2becf1cad21360900f6934e7ceb5e871a
Jens Eliasson <jens.eliasson@gmail.com>
2020-06-28 13:00:45 +0200

    updated initListener to newest code style

--- datamanager/src/main/java/eu/arrowhead/core/datamanager/DataManagerApplicationInitListener.java
+++ datamanager/src/main/java/eu/arrowhead/core/datamanager/DataManagerApplicationInitListener.java
@@ -1,58 +1,29 @@
 package eu.arrowhead.core.datamanager;
 
-import java.util.List;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import eu.arrowhead.common.ApplicationInitListener;
+import eu.arrowhead.common.core.CoreSystemService;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-import eu.arrowhead.common.ApplicationInitListener;
-import eu.arrowhead.common.CommonConstants;
-import eu.arrowhead.common.CoreCommonConstants;
-import eu.arrowhead.common.core.CoreSystemService;
-
-import eu.arrowhead.core.datamanager.service.HistorianService;
+import java.util.Base64;
+import java.util.List;
 
 @Component
 public class DataManagerApplicationInitListener extends ApplicationInitListener {
-	
-	//=================================================================================================
-	// members
-	//-------------------------------------------------------------------------------------------------
-	
-	@Autowired
-	private ApplicationContext applicationContext;
-	
-	//=================================================================================================
-	// methods
-	
-	//-------------------------------------------------------------------------------------------------
-	
-	//=================================================================================================
-	// assistant methods
-	
-	//-------------------------------------------------------------------------------------------------
-	@Override
-	protected List<CoreSystemService> getRequiredCoreSystemServiceUris() {
-		
-//		return List.of(CoreSystemService.AUTH_CONTROL_SUBSCRIPTION_SERVICE);
-		return List.of();
-	}
-	
-	//-------------------------------------------------------------------------------------------------
-	@Override
-	protected void customInit(final ContextRefreshedEvent event) {
-		logger.debug("customInit started...");	
-	}
 
-	//-------------------------------------------------------------------------------------------------
-	@Override
-	protected void customDestroy() {				
-		logger.debug("customDestroy started...");
-	}
+    //=================================================================================================
+    // members
 
+    //-------------------------------------------------------------------------------------------------
+    @Override
+    protected void customInit(final ContextRefreshedEvent event) {
+        if (sslProperties.isSslEnabled()) {
+            logger.debug("AuthInfo: {}", Base64.getEncoder().encodeToString(publicKey.getEncoded()));
+        }
+    }
+
+    @Override
+    protected List<CoreSystemService> getRequiredCoreSystemServiceUris() {
+        return List.of();
+    }
 }
-
