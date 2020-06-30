4dc5aab7d9771504971cb527f9253309df2132a2
Ingolf Gehrhardt <ingolf.gehrhardt@htw-dresden.de>
2020-06-30 14:29:58 +0200

    Added initSQL.sh (taken from the scripts/ folder) which downloads all required sql db install files Modified the README.md in order to show how to use the new file in order to get docker-all working

---
+++ docker-all/example/initSQL.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -rf sql
+
+mkdir sql
+cd sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/create_empty_arrowhead_db.sql
+sed -i 's/source /& docker-entrypoint-initdb.d\/privileges\//g' create_empty_arrowhead_db.sql
+
+mkdir privileges
+cd privileges
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/authorization_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/certificate_authority_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/choreographer_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/create_arrowhead_tables.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/device_registry_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/event_handler_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/gatekeeper_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/gateway_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/onboarding_controller_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/orchestrator_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/qos_monitor_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/service_registry_privileges.sql
+wget https://raw.githubusercontent.com/arrowhead-f/core-java-spring/master/scripts/system_registry_privileges.sql
