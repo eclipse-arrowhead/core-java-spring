a73fb53ca9fdb3c309d861962635982d1628606a
MaGaMeGa <gabormajo@yahoo.com>
2020-07-31 16:12:52 +0200

    additional implementations of  interface releted ServiceRegistryController tests

--- serviceregistry/src/test/java/eu/arrowhead/core/serviceregistry/ServiceRegistryControllerServiceInterfaceTest.java
+++ serviceregistry/src/test/java/eu/arrowhead/core/serviceregistry/ServiceRegistryControllerServiceInterfaceTest.java
@@ -3,8 +3,14 @@ package eu.arrowhead.core.serviceregistry;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.ZonedDateTime;
@@ -32,6 +38,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.arrowhead.common.database.entity.ServiceInterface;
 import eu.arrowhead.common.dto.internal.DTOConverter;
 import eu.arrowhead.common.dto.internal.ServiceInterfacesListResponseDTO;
+import eu.arrowhead.common.dto.shared.ServiceInterfaceResponseDTO;
+import eu.arrowhead.common.intf.ServiceInterfaceNameVerifier;
 import eu.arrowhead.core.serviceregistry.database.service.ServiceRegistryDBService;
 
 @RunWith(SpringRunner.class)
@@ -53,6 +61,9 @@ public class ServiceRegistryControllerServiceInterfaceTest {
 	@MockBean(name = "mockServiceRegistryDBService") 
 	private ServiceRegistryDBService serviceRegistryDBService;
 
+	@MockBean
+	private ServiceInterfaceNameVerifier interfaceValidator;
+
 	//=================================================================================================
 	// methods
 
@@ -61,9 +72,9 @@ public class ServiceRegistryControllerServiceInterfaceTest {
 	public void setup() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
 	}
-	
+
 	//=================================================================================================
-	// Tests of getServiceNames
+	// Tests of getServiceInterfaces
 
 	//-------------------------------------------------------------------------------------------------
 	@Test
@@ -81,6 +92,231 @@ public class ServiceRegistryControllerServiceInterfaceTest {
 		final ServiceInterfacesListResponseDTO responseBody = objectMapper.readValue(response.getResponse().getContentAsString(), ServiceInterfacesListResponseDTO.class);
 
 		assertEquals(numOfEntries, responseBody.getCount());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void getServiceInterfacesTestWithPageAndSizeParameter() throws Exception {
+		final int numOfEntries = 8;
+		final Page<ServiceInterface> serviceInterfaceEntries = createServiceInterfacePageForDBMocking(numOfEntries);
+		final ServiceInterfacesListResponseDTO serviceInterfaceEntriesDTO = DTOConverter.convertServiceInterfacesListToServiceInterfaceListResponseDTO(serviceInterfaceEntries);
+
+		when(serviceRegistryDBService.getServiceInterfaceEntriesResponse(anyInt(), anyInt(), any(), any())).thenReturn(serviceInterfaceEntriesDTO);
+
+		final MvcResult response = this.mockMvc.perform(get("/serviceregistry/mgmt/service/interfaces")
+											.param("page", "0")
+											.param("item_per_page", String.valueOf(numOfEntries))
+											.accept(MediaType.APPLICATION_JSON))
+											.andExpect(status().isOk())
+											.andReturn();
+		final ServiceInterfacesListResponseDTO responseBody = objectMapper.readValue(response.getResponse().getContentAsString(), ServiceInterfacesListResponseDTO.class);
+
+		assertEquals(numOfEntries, responseBody.getCount());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void getServiceInterfacesTestWithNullPageButDefinedSizeParameter() throws Exception {
+		this.mockMvc.perform(get("/serviceregistry/mgmt/service/interfaces")
+					.param("item_per_page", "1")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void getServiceInterfacesTestWithDefinedPageButNullSizeParameter() throws Exception {
+		this.mockMvc.perform(get("/serviceregistry/mgmt/service/interfaces")
+					.param("page", "0")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void getServiceInterfacesTestWithInvalidSortDirectionFlagParameter() throws Exception {
+		this.mockMvc.perform(get("/serviceregistry/mgmt/service/interfaces")
+					.param("direction", "invalid")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//=================================================================================================
+	// Tests of getServiceInterfacesById
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void getServiceInterfaceByIdTestWithExistingId() throws Exception {
+		final int requestedId = 1;
+		final ServiceInterfaceResponseDTO serviceInterfaceResponseDTO = new ServiceInterfaceResponseDTO(requestedId, "", "", "");		
+
+		when(serviceRegistryDBService.getServiceInterfaceByIdResponse(anyLong())).thenReturn(serviceInterfaceResponseDTO);
+
+		final MvcResult response = this.mockMvc.perform(get("/serviceregistry/mgmt/service/interfaces/" + requestedId)
+												.accept(MediaType.APPLICATION_JSON))
+												.andExpect(status().isOk())
+												.andReturn();
+		final ServiceInterfaceResponseDTO responseBody = objectMapper.readValue(response.getResponse().getContentAsString(), ServiceInterfaceResponseDTO.class);
+
+		assertEquals(requestedId, responseBody.getId());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void getServiceInterfaceByIdTestWithInvalidId() throws Exception {
+		this.mockMvc.perform(get("/serviceregistry/mgmt/service/interfaces/-1")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//=================================================================================================
+	// Tests of addServiceInterface
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void addServiceInterfaceTestWithValidInterface() throws Exception {
+		final String serviceInterface = "testInterface";
+		final ServiceInterfaceResponseDTO serviceInterfaceResponseDTO = new ServiceInterfaceResponseDTO(0, serviceInterface,"","");
+
+		when(interfaceValidator.isValid(anyString())).thenReturn(Boolean.TRUE);
+		when(serviceRegistryDBService.createServiceInterfaceResponse(anyString())).thenReturn(serviceInterfaceResponseDTO);
+
+		final MvcResult response = this.mockMvc.perform(post("/serviceregistry/mgmt/service/interfaces")
+												.content("{\"serviceInterface\": \"" + serviceInterface + "\"}")
+												.contentType(MediaType.APPLICATION_JSON)
+												.accept(MediaType.APPLICATION_JSON))
+												.andExpect(status().isCreated())
+												.andReturn();
+		final ServiceInterfaceResponseDTO responseBody = objectMapper.readValue(response.getResponse().getContentAsString(), ServiceInterfaceResponseDTO.class);
+
+		assertEquals(serviceInterface, responseBody.getInterfaceName());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void addServiceInterfaceTestWithNullInterface() throws Exception {
+		this.mockMvc.perform(post("/serviceregistry/mgmt/service/interfaces")
+					.content("{}")
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void addServiceInterfaceTestWithBlankInterface() throws Exception {
+		this.mockMvc.perform(post("/serviceregistry/mgmt/service/interfaces")
+					.content("{\"serviceInterface\": \"      \"}")
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//=================================================================================================
+	// Tests of putUpdateServiceInterface
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void putUpdateServiceInterfaceTestWithValidInterface() throws Exception {
+		final String serviceInterface = "testInterface";
+		final int id = 5;
+		final ServiceInterfaceResponseDTO serviceInterfaceResponseDTO = new ServiceInterfaceResponseDTO(id, serviceInterface,"","");
+
+		when(interfaceValidator.isValid(anyString())).thenReturn(Boolean.TRUE);
+		when(serviceRegistryDBService.updateServiceInterfaceByIdResponse(anyLong(), anyString())).thenReturn(serviceInterfaceResponseDTO);
+		
+		final MvcResult response = this.mockMvc.perform(put("/serviceregistry/mgmt/service/interfaces/" + id)
+												.content("{\"serviceInterface\": \"" + serviceInterface + "\"}")
+												.contentType(MediaType.APPLICATION_JSON)
+												.accept(MediaType.APPLICATION_JSON))
+												.andExpect(status().isOk())
+												.andReturn();
+		final ServiceInterfaceResponseDTO responseBody = objectMapper.readValue(response.getResponse().getContentAsString(), ServiceInterfaceResponseDTO.class);
+
+		assertEquals(serviceInterface, responseBody.getInterfaceName());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void putUpdateServiceInterfaceTestWithNullInterface() throws Exception {
+		this.mockMvc.perform(put("/serviceregistry/mgmt/service/interfaces/5")
+					.content("{}")
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void putUpdateServiceInterfaceTestWithBlankInterface() throws Exception {
+		this.mockMvc.perform(put("/serviceregistry/mgmt/service/interfaces/5")
+					.content("{\"serviceInterface\": \"     \"}")
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//=================================================================================================
+	// Tests of patchUpdateServiceInterface
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void patchUpdateServiceInterfaceTestWithValidInterface() throws Exception {
+		final String serviceInterface = "testInterface";
+		final int id = 5;
+		final ServiceInterfaceResponseDTO serviceInterfaceResponseDTO = new ServiceInterfaceResponseDTO(id, serviceInterface,"","");
+
+		when(interfaceValidator.isValid(anyString())).thenReturn(Boolean.TRUE);
+		when(serviceRegistryDBService.updateServiceInterfaceByIdResponse(anyLong(), anyString())).thenReturn(serviceInterfaceResponseDTO);
+
+		final MvcResult response = this.mockMvc.perform(patch("/serviceregistry/mgmt/service/interfaces/" + id)
+												.content("{\"serviceInterface\": \"" + serviceInterface + "\"}")
+												.contentType(MediaType.APPLICATION_JSON)
+												.accept(MediaType.APPLICATION_JSON))
+												.andExpect(status().isOk())
+												.andReturn();
+		final ServiceInterfaceResponseDTO responseBody = objectMapper.readValue(response.getResponse().getContentAsString(), ServiceInterfaceResponseDTO.class);
+
+		assertEquals(serviceInterface, responseBody.getInterfaceName());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void patchUpdateServiceInterfaceTestWithNullInterface() throws Exception {
+		this.mockMvc.perform(patch("/serviceregistry/mgmt/service/interfaces/5")
+					.content("{}")
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void patchUpdateServiceInterfaceTestWithBlankInterface() throws Exception {
+		this.mockMvc.perform(patch("/serviceregistry/mgmt/service/interfaces/5")
+					.content("{\"serviceInterface\": \"      \"}")
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
+	}
+
+	//=================================================================================================
+	// Tests of removeServiceInterface
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void removeServiceInterfaceTestWithValidId( ) throws Exception {
+		this.mockMvc.perform(delete("/serviceregistry/mgmt/service/interfaces/4")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void removeServiceInterfaceTestWithInvalidId( ) throws Exception {
+		this.mockMvc.perform(delete("/serviceregistry/mgmt/service/interfaces/0")
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isBadRequest());
 	}
 
 	//=================================================================================================
