060635bcbe6cd8044f28424581fc9a2072e0c608
Jens Eliasson <jens.eliasson@gmail.com>
2021-03-28 11:54:53 +0200

    code cleanup

--- configuration/src/main/java/eu/arrowhead/core/configuration/security/ConfigurationAccessControlFilter.java
+++ configuration/src/main/java/eu/arrowhead/core/configuration/security/ConfigurationAccessControlFilter.java
@@ -15,18 +15,13 @@ package eu.arrowhead.core.configuration.security;
 
 import eu.arrowhead.common.CommonConstants;
 import eu.arrowhead.common.CoreCommonConstants;
-//import eu.arrowhead.common.SecurityUtilities;
-//import eu.arrowhead.common.dto.shared.CertificateType;
 import eu.arrowhead.common.security.CoreSystemAccessControlFilter;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import eu.arrowhead.common.Utilities;
-//import eu.arrowhead.common.core.CoreSystem;
 import eu.arrowhead.common.exception.AuthException;
 
 import java.util.Map;
@@ -45,7 +40,6 @@ public class ConfigurationAccessControlFilter extends CoreSystemAccessControlFil
 	@Override
 	protected void checkClientAuthorized(final String clientCN, final String method, final String requestTarget, final String requestJSON, final Map<String,String[]> queryParams) {
 		super.checkClientAuthorized(clientCN, method, requestTarget, requestJSON, queryParams);
-
                 final String cloudCN = getServerCloudCN();
 
                 if (requestTarget.contains(CoreCommonConstants.MGMT_URI)) {
