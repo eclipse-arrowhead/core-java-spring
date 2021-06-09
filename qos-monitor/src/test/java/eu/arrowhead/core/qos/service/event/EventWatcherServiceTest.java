0c5ef880f205f30e26dcb4c193d94c6defc0b05e
MaGaMeGa <gabormajo@yahoo.com>
2021-06-09 04:11:08 -0400

    implemented EventWatcherService tests

--- qos-monitor/src/test/java/eu/arrowhead/core/qos/service/event/EventWatcherServiceTest.java
+++ qos-monitor/src/test/java/eu/arrowhead/core/qos/service/event/EventWatcherServiceTest.java
@@ -1,5 +1,12 @@
 package eu.arrowhead.core.qos.service.event;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.concurrent.LinkedBlockingQueue;
 
 import org.junit.Test;
@@ -9,6 +16,7 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import eu.arrowhead.common.dto.shared.EventDTO;
+import eu.arrowhead.common.exception.ArrowheadException;
 import eu.arrowhead.common.exception.InvalidParameterException;
 
 @RunWith(SpringRunner.class)
@@ -29,6 +37,31 @@ public class EventWatcherServiceTest {
 	//-------------------------------------------------------------------------------------------------
 	@Test(expected = InvalidParameterException.class)
 	public void testPutEventToQueueNullEvent() {
+
 		eventWatcherService.putEventToQueue(null);
+
+		verify(eventQueue, never()).add(any());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test(expected = ArrowheadException.class)
+	public void testPutEventToQueueEventQueueAddThrowsException() {
+
+		doThrow(new IllegalStateException()).when(eventQueue).add(any());
+
+		eventWatcherService.putEventToQueue(new EventDTO());
+
+		verify(eventQueue, times(1)).add(any());
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void testPutEventToQueueOk() {
+
+		when(eventQueue.add(any())).thenReturn(true);
+
+		eventWatcherService.putEventToQueue(new EventDTO());
+
+		verify(eventQueue, times(1)).add(any());
 	}
 }
