6826a0db4efb5cfcea0d483103cde3bec595f15a
MaGaMeGa <gmajoros@aitia.ai>
2021-06-10 05:34:43 -0400

    additional PingEventProcessor tests

--- qos-monitor/src/test/java/eu/arrowhead/core/qos/service/ping/monitor/PingEventProcessorTest.java
+++ qos-monitor/src/test/java/eu/arrowhead/core/qos/service/ping/monitor/PingEventProcessorTest.java
@@ -3,9 +3,12 @@ package eu.arrowhead.core.qos.service.ping.monitor;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.ZonedDateTime;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -15,7 +18,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import eu.arrowhead.common.Utilities;
+import eu.arrowhead.common.dto.shared.EventDTO;
+import eu.arrowhead.common.dto.shared.QosMonitorEventType;
 import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.core.qos.QosMonitorConstants;
+import eu.arrowhead.core.qos.dto.event.EventDTOConverter;
 
 @RunWith(SpringRunner.class)
 public class PingEventProcessorTest {
@@ -23,7 +31,7 @@ public class PingEventProcessorTest {
 	//=================================================================================================
 	// members
 	@InjectMocks
-	private PingEventProcessor pingEventProcessor;
+	private final PingEventProcessor pingEventProcessor = new PingEventProcessor();
 
 	@Mock
 	private ConcurrentHashMap<UUID, PingEventBufferElement> eventBuffer;
@@ -38,9 +46,11 @@ public class PingEventProcessorTest {
 		final UUID processId = null;
 		final long measurementExpiryTime = 1L;
 
+		when(eventBuffer.get(any())).thenReturn(null);
+
 		pingEventProcessor.processEvents(processId, measurementExpiryTime);
 
-		verify(eventBuffer, never()).get(any());
+		verify(eventBuffer,  never()).get(any());
 	}
 
 	//-------------------------------------------------------------------------------------------------
@@ -86,19 +96,98 @@ public class PingEventProcessorTest {
 
 	//-------------------------------------------------------------------------------------------------
 	@Test(expected = ArrowheadException.class)
-	public void testProcessEventsInterrruptedEvent() {
+	public void testProcessEventsInterrruptedEventPresent() {
 
 		final UUID processId = UUID.randomUUID();
 		final long measurementExpiryTime = System.currentTimeMillis() + 10000;
 
 		final PingEventBufferElement bufferElement = new PingEventBufferElement(processId);
-		
+		bufferElement.addEvent(
+				QosMonitorConstants.INTERRUPTED_MONITORING_MEASUREMENT_EVENT_POSITION,
+				EventDTOConverter.convertToInterruptedMonitoringMeasurementEvent(getValidInterruptedDTOForTest()));
 
 		when(eventBuffer.get(any())).thenReturn(bufferElement);
 
 		pingEventProcessor.processEvents(processId, measurementExpiryTime);
 
-		verify(eventBuffer, atLeastOnce()).get(any());
-		verify(eventBuffer, never()).remove(any());
+		verify(eventBuffer, times(1)).get(any());
+		verify(eventBuffer, times(1)).remove(any());
+	}
+
+	//=================================================================================================
+	// assistant methods
+
+	//-------------------------------------------------------------------------------------------------
+	private EventDTO getInValidEventTypeEventDTOForTest() {
+
+		final EventDTO event = new EventDTO();
+		event.setEventType("UNKNOWN_MEAUSREMENT_EVENT");
+		event.setMetaData(getValidMeasuermentEventDTOMetadtaProcessIdForTest());
+		event.setPayload(getValidMeasuermentEventDTOEmptyPayloadForTest());
+		event.setTimeStamp(Utilities.convertZonedDateTimeToUTCString(ZonedDateTime.now()));
+
+		return event;
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	private EventDTO getValidReceivedMeasurementRequestEventDTOForTest() {
+
+		final EventDTO event = new EventDTO();
+		event.setEventType(QosMonitorEventType.RECEIVED_MONITORING_REQUEST.name());
+		event.setMetaData(getValidMeasuermentEventDTOMetadtaProcessIdForTest());
+		event.setPayload(getValidMeasuermentEventDTOEmptyPayloadForTest());
+		event.setTimeStamp(Utilities.convertZonedDateTimeToUTCString(ZonedDateTime.now()));
+
+		return event;
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	private EventDTO getValidStartEventDTOForTest() {
+
+		final EventDTO event = new EventDTO();
+		event.setEventType(QosMonitorEventType.STARTED_MONITORING_MEASUREMENT.name());
+		event.setMetaData(getValidMeasuermentEventDTOMetadtaProcessIdForTest());
+		event.setPayload(getValidMeasuermentEventDTOEmptyPayloadForTest());
+		event.setTimeStamp(Utilities.convertZonedDateTimeToUTCString(ZonedDateTime.now()));
+
+		return event;
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	private EventDTO getValidFinishedEventDTOForTest() {
+
+		final EventDTO event = new EventDTO();
+		event.setEventType(QosMonitorEventType.FINISHED_MONITORING_MEASUREMENT.name());
+		event.setMetaData(getValidMeasuermentEventDTOMetadtaProcessIdForTest());
+		event.setPayload(getValidMeasuermentEventDTOEmptyPayloadForTest());
+		event.setTimeStamp(Utilities.convertZonedDateTimeToUTCString(ZonedDateTime.now()));
+
+		return event;
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	private EventDTO getValidInterruptedDTOForTest() {
+
+		final EventDTO event = new EventDTO();
+		event.setEventType(QosMonitorEventType.INTERRUPTED_MONITORING_MEASUREMENT.name());
+		event.setMetaData(getValidMeasuermentEventDTOMetadtaProcessIdForTest());
+		event.setPayload(getValidMeasuermentEventDTOEmptyPayloadForTest());
+		event.setTimeStamp(Utilities.convertZonedDateTimeToUTCString(ZonedDateTime.now()));
+
+		return event;
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	private Map<String, String> getValidMeasuermentEventDTOMetadtaProcessIdForTest() {
+
+		return Map.of(QosMonitorConstants.PROCESS_ID_KEY, UUID.randomUUID().toString());
+
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	private String getValidMeasuermentEventDTOEmptyPayloadForTest() {
+
+		return "[]";
+
 	}
 }
