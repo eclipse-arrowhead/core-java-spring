883652961957b1a07092b5821825fc9d233c3815
Lukas Römer <lukas.roemer@bosch.io>
2021-09-01 15:49:08 +0200

    Change log level in HawkBitConsumer

--- configuration-manager/src/main/java/eu/arrowhead/core/confmgr/hawkbit/HawkbitDmfConsumer.java
+++ configuration-manager/src/main/java/eu/arrowhead/core/confmgr/hawkbit/HawkbitDmfConsumer.java
@@ -112,7 +112,7 @@ public class HawkbitDmfConsumer extends DefaultConsumer {
     @Override
     public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
         try {
-            log.info("Received message");
+            log.debug("Received message");
             handleMessage(envelope, properties, body);
         } catch (IOException e) {
             log.atError().withThrowable(e).log(
