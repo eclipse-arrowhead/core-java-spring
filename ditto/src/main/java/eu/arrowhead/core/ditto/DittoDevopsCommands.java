b1b4422742098c8262427e8c55a5ca86c4cd99fd
Nicklas Nyström <nicklas.nystrom@xarepo.com>
2022-03-25 09:36:32 +0100

    Minor cleanup

--- ditto/src/main/java/eu/arrowhead/core/ditto/DittoDevopsCommands.java
+++ ditto/src/main/java/eu/arrowhead/core/ditto/DittoDevopsCommands.java
@@ -11,7 +11,6 @@
 
 package eu.arrowhead.core.ditto;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
