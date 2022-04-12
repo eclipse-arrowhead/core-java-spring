838f1e893e934628a85d670c8308a798a251b1f9
Nicklas Nyström <nicklas.nystrom@xarepo.com>
2022-04-12 14:42:35 +0200

    Fix Ditto tests

--- ditto/src/test/java/eu/arrowhead/core/ditto/DittoMonitorControllerTest.java
+++ ditto/src/test/java/eu/arrowhead/core/ditto/DittoMonitorControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -23,6 +24,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = {DittoTestContext.class})
 public class DittoMonitorControllerTest {
 
 	//=================================================================================================
---
+++ ditto/src/test/java/eu/arrowhead/core/ditto/StandaloneModeInDittoTests.java
@@ -0,0 +1,18 @@
+/********************************************************************************
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Arrowhead Consortia - conceptualization
+ ********************************************************************************/
+
+package eu.arrowhead.core.ditto;
+
+import eu.arrowhead.common.testhelper.StandaloneModeInTests;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StandaloneModeInDittoTests extends StandaloneModeInTests {}
