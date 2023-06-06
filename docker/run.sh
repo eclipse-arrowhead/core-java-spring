77be7480b33e62379e20a689b0c7262c3b898e12
Tamas Bordi <borditamas@gmail.com>
2023-06-06 08:56:35 +0200

    Docker rework (finished)

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 cd /opt/arrowhead
-if [ -f "/opt/arrowhead/application.jar" ]; then
-    echo "Executable exists."
+if [ -z "$SYSTEM_NAME" ]; then
+    echo "The environment variable is not set."
 else
-    mv /opt/arrowhead-temp/$SYSTEM_NAME/target/arrowhead-$SYSTEM_NAME-$AH_VERSION.jar /opt/arrowhead/application.jar
-    rm -rf /opt/arrowhead-temp
+    echo "Arrowhead Core System: $SYSTEM_NAME"
+	if [ -f "/opt/arrowhead/application.jar" ]; then
+		echo "Container has been already initialized."
+	else
+		mv /opt/arrowhead-temp/$SYSTEM_NAME/target/arrowhead-$SYSTEM_NAME-$AH_VERSION.jar /opt/arrowhead/application.jar
+		rm -rf /opt/arrowhead-temp
+		echo "Container has been initialized."
+	fi
+	java -jar application.jar
 fi
-java -jar application.jar
+
 
